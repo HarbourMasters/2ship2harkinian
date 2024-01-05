3e96624355b073835504c049dd7286944a9a03c2
Garrett Cox <garrettjcox@gmail.com>
2024-01-05 10:14:11 -0600

    Delete irrelevant files from decomp

--- mm/diff_settings.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-def apply(config, args):
-    config['baseimg'] = 'baserom_uncompressed.z64'
-    config['myimg'] = 'mm.us.rev1.rom_uncompressed.z64'
-    config['mapfile'] = 'build/mm.map'
-    config['source_directories'] = ['./src','./include']
-    config['objdump_flags'] = ['-M','reg-names=32']
-    config['makeflags'] = ['KEEP_MDEBUG=1']
--- mm/tools/fado/include/mido.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#include "vc_vector/vc_vector.h"
-
-int Mido_WriteDependencyFile(FILE* dependencyFile, const char* relocFile, vc_vector* inputFilesVector);
