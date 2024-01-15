91a05660bac0564641486cb3297fc7f3eb0106ad
Garrett Cox <garrettjcox@gmail.com>
2024-01-15 05:17:26 +0000

    Add support for json save files (#54)

--- mm/2s2h/BenPort.cpp
+++ mm/2s2h/BenPort.cpp
@@ -30,6 +30,7 @@
 #include "z64.h"
 #include "macros.h"
 #include <Utils/StringHelper.h>
+#include <nlohmann/json.hpp>
 
 #include <Fast3D/gfx_pc.h>
 #include <Fast3D/gfx_rendering_api.h>
@@ -51,6 +52,7 @@ CrowdControl* CrowdControl::Instance;
 
 #include <libultraship/libultraship.h>
 #include <BenGui.hpp>
+#include "BenJsonConversions.hpp"
 
 #include "Enhancements/controls/SohInputEditorWindow.h"
 
@@ -1383,4 +1385,194 @@ extern "C" int Controller_ShouldRumble(size_t slot) {
     }
 
     return 0;
+}
+
+// This entire thing is temporary until we have a more robust save system that
+// supports backwards compatability, migrations, threaded saving, save sections, etc.
+typedef enum FlashSlotFile {
+    /* -1 */ FLASH_SLOT_FILE_UNAVAILABLE = -1,
+    /*  0 */ FLASH_SLOT_FILE_1_NEW_CYCLE,
+    /*  1 */ FLASH_SLOT_FILE_1_NEW_CYCLE_BACKUP,
+    /*  2 */ FLASH_SLOT_FILE_2_NEW_CYCLE,
+    /*  3 */ FLASH_SLOT_FILE_2_NEW_CYCLE_BACKUP,
+    /*  4 */ FLASH_SLOT_FILE_1_OWL_SAVE,
+    /*  5 */ FLASH_SLOT_FILE_1_OWL_SAVE_BACKUP,
+    /*  6 */ FLASH_SLOT_FILE_2_OWL_SAVE,
+    /*  7 */ FLASH_SLOT_FILE_2_OWL_SAVE_BACKUP,
+    /*  8 */ FLASH_SLOT_FILE_SRAM_HEADER,
+    /*  9 */ FLASH_SLOT_FILE_SRAM_HEADER_BACKUP,
+} FlashSlotFile;
+
+#define GET_NEWF(save, index) (save.saveInfo.playerData.newf[index])
+#define IS_VALID_FILE(save)                                      \
+    ((GET_NEWF(save, 0) == 'Z') && (GET_NEWF(save, 1) == 'E') && \
+     (GET_NEWF(save, 2) == 'L') && (GET_NEWF(save, 3) == 'D') && \
+     (GET_NEWF(save, 4) == 'A') && (GET_NEWF(save, 5) == '3'))
+
+const std::filesystem::path savesFolderPath(LUS::Context::GetPathRelativeToAppDirectory("Save"));
+
+void WriteSaveFile(std::filesystem::path fileName, nlohmann::json j) {
+    const std::filesystem::path filePath = savesFolderPath / fileName;
+
+    if (!std::filesystem::exists(savesFolderPath)) {
+        std::filesystem::create_directory(savesFolderPath);
+    }
+
+    std::ofstream o(filePath);
+    o << std::setw(4) << j << std::endl;
+    o.close();
+}
+
+void DeleteSaveFile(std::filesystem::path fileName) {
+    const std::filesystem::path filePath = savesFolderPath / fileName;
+
+    if (std::filesystem::exists(filePath)) {
+        std::filesystem::remove(filePath);
+    }
+}
+
+int ReadSaveFile(std::filesystem::path fileName, nlohmann::json& j) {
+    const std::filesystem::path filePath = savesFolderPath / fileName;
+
+    if (!std::filesystem::exists(filePath)) {
+        return -1;
+    }
+
+    std::ifstream i(filePath);
+    i >> j;
+    i.close();
+    return 0;
+}
+
+extern "C" void BenSysFlashrom_WriteData(u8* saveBuffer, u32 pageNum, u32 pageCount) {
+    FlashSlotFile flashSlotFile = FLASH_SLOT_FILE_UNAVAILABLE;
+    bool isBackup = false;
+    for (u32 i = 0; i < ARRAY_COUNT(gFlashSaveStartPages) - 1; i++) {
+        if (pageNum == gFlashSaveStartPages[i]) {
+            flashSlotFile = static_cast<FlashSlotFile>(i);
+            break;
+        }
+    }
+
+    if (flashSlotFile == FLASH_SLOT_FILE_UNAVAILABLE) {
+        return;
+    }
+
+    switch (flashSlotFile) {
+        case FLASH_SLOT_FILE_1_NEW_CYCLE_BACKUP:
+        case FLASH_SLOT_FILE_2_NEW_CYCLE_BACKUP:
+            isBackup = true;
+            // fallthrough
+        case FLASH_SLOT_FILE_1_NEW_CYCLE:
+        case FLASH_SLOT_FILE_2_NEW_CYCLE: {
+            Save save;
+            memcpy(&save, saveBuffer, sizeof(Save));
+
+            std::string fileName = "save_" + std::to_string(flashSlotFile) + ".sav";
+            if (isBackup) fileName += ".bak";
+        
+            if (IS_VALID_FILE(save)) {
+                WriteSaveFile(fileName, save);
+            } else {
+                DeleteSaveFile(fileName);
+            }
+            break;
+        }
+        case FLASH_SLOT_FILE_1_OWL_SAVE_BACKUP:
+        case FLASH_SLOT_FILE_2_OWL_SAVE_BACKUP:
+            isBackup = true;
+            // fallthrough
+        case FLASH_SLOT_FILE_1_OWL_SAVE:
+        case FLASH_SLOT_FILE_2_OWL_SAVE: {
+            SaveContext saveContext;
+            memcpy(&saveContext, saveBuffer, sizeof(SaveContext));
+
+            std::string fileName = "save_" + std::to_string(flashSlotFile) + ".sav";
+            if (isBackup) fileName += ".bak";
+
+            if (IS_VALID_FILE(saveContext.save)) {
+                WriteSaveFile(fileName, saveContext);
+            } else {
+                DeleteSaveFile(fileName);
+            }
+            break;
+        }
+        case FLASH_SLOT_FILE_SRAM_HEADER_BACKUP:
+        case FLASH_SLOT_FILE_SRAM_HEADER: {
+            SaveOptions saveOptions;
+            memcpy(&saveOptions, saveBuffer, sizeof(SaveOptions));
+
+            std::string fileName = "global.sav";
+            WriteSaveFile(fileName, saveOptions);
+            break;
+        }
+    }
+}
+
+extern "C" s32 BenSysFlashrom_ReadData(void* saveBuffer, u32 pageNum, u32 pageCount) {
+    FlashSlotFile flashSlotFile = FLASH_SLOT_FILE_UNAVAILABLE;
+    bool isBackup = false;
+    for (u32 i = 0; i < ARRAY_COUNT(gFlashSaveStartPages) - 1; i++) {
+        if (pageNum == gFlashSaveStartPages[i]) {
+            flashSlotFile = static_cast<FlashSlotFile>(i);
+            break;
+        }
+    }
+
+    if (flashSlotFile == FLASH_SLOT_FILE_UNAVAILABLE) {
+        return -1;
+    }
+
+    switch (flashSlotFile) {
+        case FLASH_SLOT_FILE_1_NEW_CYCLE_BACKUP:
+        case FLASH_SLOT_FILE_2_NEW_CYCLE_BACKUP:
+            isBackup = true;
+            // fallthrough
+        case FLASH_SLOT_FILE_1_NEW_CYCLE:
+        case FLASH_SLOT_FILE_2_NEW_CYCLE: {
+            std::string fileName = "save_" + std::to_string(flashSlotFile) + ".sav";
+            if (isBackup) fileName += ".bak";
+
+            nlohmann::json j;
+            int result = ReadSaveFile(fileName, j);
+            if (result != 0) return result;
+
+            Save save = j;
+
+            memcpy(saveBuffer, &save, sizeof(Save));
+            return result;
+        }
+        case FLASH_SLOT_FILE_1_OWL_SAVE_BACKUP:
+        case FLASH_SLOT_FILE_2_OWL_SAVE_BACKUP:
+            isBackup = true;
+            // fallthrough
+        case FLASH_SLOT_FILE_1_OWL_SAVE:
+        case FLASH_SLOT_FILE_2_OWL_SAVE: {
+            std::string fileName = "save_" + std::to_string(flashSlotFile) + ".sav";
+            if (isBackup) fileName += ".bak";
+
+            nlohmann::json j;
+            int result = ReadSaveFile(fileName, j);
+            if (result != 0) return result;
+
+            SaveContext saveContext = j;
+
+            memcpy(saveBuffer, &saveContext, sizeof(SaveContext));
+            return result;
+        }
+        case FLASH_SLOT_FILE_SRAM_HEADER:
+        case FLASH_SLOT_FILE_SRAM_HEADER_BACKUP: {
+            std::string fileName = "global.sav";
+
+            nlohmann::json j;
+            int result = ReadSaveFile(fileName, j);
+            if (result != 0) return result;
+
+            SaveOptions saveOptions = j;
+
+            memcpy(saveBuffer, &saveOptions, sizeof(SaveOptions));
+            return 0;
+            break;
+        }
+    }
 }
--- mm/src/code/title_setup.c
+++ mm/src/code/title_setup.c
@@ -49,8 +49,7 @@ void Setup_SetRegs(void) {
 
 void Setup_InitImpl(SetupState* this) {
     SysFlashrom_InitFlash();
-    // BENTODO: this doesn't crash but was stubbed in minibuild? probably just for debug purposes
-    // SaveContext_Init();
+    SaveContext_Init();
     Setup_SetRegs();
 
     STOP_GAMESTATE(&this->state);
