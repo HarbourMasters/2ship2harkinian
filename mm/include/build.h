e544c3236b43099eb72b6b9940ef82e7d6b0cd9c
Adam Bird <archez39@me.com>
2024-03-03 20:59:56 -0500

    add build version, custom assets folder, and fix extraction

--- mm/include/build.h
+++ mm/include/build.h
@@ -1,6 +1,14 @@
 #ifndef BUILD_H
 #define BUILD_H
 
+#include <libultraship/libultra.h>
+
+// 2S2H [Port] Version information
+extern char gBuildVersion[];
+extern u16 gBuildVersionMajor;
+extern u16 gBuildVersionMinor;
+extern u16 gBuildVersionPatch;
+
 extern char gBuildTeam[];
 extern char gBuildDate[];
 extern char gBuildMakeOption[];
