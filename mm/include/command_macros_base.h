0a0898617608f362660fa2f5f0f86452dc2d6c37
Archez <Archez@users.noreply.github.com>
2024-04-15 23:23:35 -0400

    Fix song of double time camera spline data (#203)

--- mm/include/command_macros_base.h
+++ mm/include/command_macros_base.h
@@ -6,6 +6,9 @@
  * Each macro packs bytes (B), halfwords (H) and words (W, for consistency) into a single word
  */
 
+// 2S2H [Port] Order B and H variables for endianness
+#ifdef IS_BIGENDIAN
+
 #define CMD_BBBB(a, b, c, d) (_SHIFTL(a, 24, 8) | _SHIFTL(b, 16, 8) | _SHIFTL(c, 8, 8) | _SHIFTL(d, 0, 8))
 
 #define CMD_BBH(a, b, c) (_SHIFTL(a, 24, 8) | _SHIFTL(b, 16, 8) | _SHIFTL(c, 0, 16))
@@ -13,6 +16,18 @@
 #define CMD_HBB(a, b, c) (_SHIFTL(a, 16, 16) | _SHIFTL(b, 8, 8) | _SHIFTL(c, 0, 8))
 
 #define CMD_HH(a, b) (_SHIFTL(a, 16, 16) | _SHIFTL(b, 0, 16))
+
+#else
+
+#define CMD_BBBB(a, b, c, d) (_SHIFTL(a, 0, 8) | _SHIFTL(b, 8, 8) | _SHIFTL(c, 16, 8) | _SHIFTL(d, 24, 8))
+
+#define CMD_BBH(a, b, c) (_SHIFTL(a, 0, 8) | _SHIFTL(b, 8, 8) | _SHIFTL(c, 16, 16))
+
+#define CMD_HBB(a, b, c) (_SHIFTL(a, 0, 16) | _SHIFTL(b, 16, 8) | _SHIFTL(c, 24, 8))
+
+#define CMD_HH(a, b) (_SHIFTL(a, 0, 16) | _SHIFTL(b, 16, 16))
+
+#endif
 
 #define CMD_W(a) (a)
 
