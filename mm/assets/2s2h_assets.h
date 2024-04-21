dc2d311de0eed2fa28ec822d7bd66af8fe59c819
PurpleHato <linkvssangoku.jr@gmail.com>
2024-04-22 01:43:09 +0200

    ADD: 24 Hours Clock + .gitignore on the 2s2h.h (#229)
    * ADD: 24 Hours Clock
    
    * TWEAK: gitignore

---
+++ mm/assets/2s2h_assets.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "align_asset_macro.h"
+
+// This file is manually made
+// When new assets are added to the soh.otr file
+// We need to add the aligned version of the resource names here and use in code
+// On Mac, not using aligned resource names was causing crashes in release builds
+
+// textures
+#define dgDPad "__OTR__textures/parameter_static/gDPad"
+static const ALIGN_ASSET(2) char gDPadTex[] = dgDPad;
+
+#define dgArrowUp "__OTR__textures/parameter_static/gArrowUp"
+static const ALIGN_ASSET(2) char gArrowUpTex[] = dgArrowUp;
+
+#define dgArrowDown "__OTR__textures/parameter_static/gArrowDown"
+static const ALIGN_ASSET(2) char gArrowDownTex[] = dgArrowDown;
+
+#define dgTriforcePiece "__OTR__textures/parameter_static/gTriforcePiece"
+static const ALIGN_ASSET(2) char gTriforcePieceTex[] = dgTriforcePiece;
+
+#define dgThreeDayClockHour13Tex "__OTR__textures/parameter_static/gThreeDayClockHour13Tex"
+static const ALIGN_ASSET(2) char gThreeDayClockHour13Tex[] = dgThreeDayClockHour13Tex;
+
+#define dgThreeDayClockHour14Tex "__OTR__textures/parameter_static/gThreeDayClockHour14Tex"
+static const ALIGN_ASSET(2) char gThreeDayClockHour14Tex[] = dgThreeDayClockHour14Tex;
+
+#define dgThreeDayClockHour15Tex "__OTR__textures/parameter_static/gThreeDayClockHour15Tex"
+static const ALIGN_ASSET(2) char gThreeDayClockHour15Tex[] = dgThreeDayClockHour15Tex;
+
+#define dgThreeDayClockHour16Tex "__OTR__textures/parameter_static/gThreeDayClockHour16Tex"
+static const ALIGN_ASSET(2) char gThreeDayClockHour16Tex[] = dgThreeDayClockHour16Tex;
+
+#define dgThreeDayClockHour17Tex "__OTR__textures/parameter_static/gThreeDayClockHour17Tex"
+static const ALIGN_ASSET(2) char gThreeDayClockHour17Tex[] = dgThreeDayClockHour17Tex;
+
+#define dgThreeDayClockHour18Tex "__OTR__textures/parameter_static/gThreeDayClockHour18Tex"
+static const ALIGN_ASSET(2) char gThreeDayClockHour18Tex[] = dgThreeDayClockHour18Tex;
+
+#define dgThreeDayClockHour19Tex "__OTR__textures/parameter_static/gThreeDayClockHour19Tex"
+static const ALIGN_ASSET(2) char gThreeDayClockHour19Tex[] = dgThreeDayClockHour19Tex;
+
+#define dgThreeDayClockHour20Tex "__OTR__textures/parameter_static/gThreeDayClockHour20Tex"
+static const ALIGN_ASSET(2) char gThreeDayClockHour20Tex[] = dgThreeDayClockHour20Tex;
+
+#define dgThreeDayClockHour21Tex "__OTR__textures/parameter_static/gThreeDayClockHour21Tex"
+static const ALIGN_ASSET(2) char gThreeDayClockHour21Tex[] = dgThreeDayClockHour21Tex;
+
+#define dgThreeDayClockHour22Tex "__OTR__textures/parameter_static/gThreeDayClockHour22Tex"
+static const ALIGN_ASSET(2) char gThreeDayClockHour22Tex[] = dgThreeDayClockHour22Tex;
+
+#define dgThreeDayClockHour23Tex "__OTR__textures/parameter_static/gThreeDayClockHour23Tex"
+static const ALIGN_ASSET(2) char gThreeDayClockHour23Tex[] = dgThreeDayClockHour23Tex;
+
+#define dgThreeDayClockHour24Tex "__OTR__textures/parameter_static/gThreeDayClockHour24Tex"
+static const ALIGN_ASSET(2) char gThreeDayClockHour24Tex[] = dgThreeDayClockHour24Tex;
