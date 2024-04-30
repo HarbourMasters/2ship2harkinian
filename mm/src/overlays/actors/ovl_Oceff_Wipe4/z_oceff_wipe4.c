3ab93fd9b677cf7b97f143a1fa1e99ab27b3de62
inspectredc <inspectredc@gmail.com>
2024-04-30 10:25:12 +0100

    Fix Scarecrow Song

--- mm/src/overlays/actors/ovl_Oceff_Wipe4/z_oceff_wipe4.c
+++ mm/src/overlays/actors/ovl_Oceff_Wipe4/z_oceff_wipe4.c
@@ -114,10 +114,15 @@ void OceffWipe4_Draw(Actor* thisx, PlayState* play) {
         gSPDisplayList(POLY_XLU_DISP++, sScarecrowSongMaterialDL);
     }
 
-    gSPDisplayList(POLY_XLU_DISP++, sScarecrowSongModelDL);
+    // #region 2S2H [Port]
+    // We need to first load the DL before we can index it on the port
+    Gfx* scarecrowSongModelDL = ResourceMgr_LoadTexOrDListByName(sScarecrowSongModelDL);
+
+    gSPDisplayList(POLY_XLU_DISP++, scarecrowSongModelDL);
     gSPDisplayList(POLY_XLU_DISP++, Gfx_TwoTexScroll(play->state.gfxCtx, G_TX_RENDERTILE, scroll * 2, scroll * -2, 32,
                                                      64, 1, scroll * -1, scroll, 32, 32));
-    gSPDisplayList(POLY_XLU_DISP++, &sScarecrowSongModelDL[11]);
+    gSPDisplayList(POLY_XLU_DISP++, &scarecrowSongModelDL[11]);
+    // #endregion
 
     CLOSE_DISPS(play->state.gfxCtx);
 }
