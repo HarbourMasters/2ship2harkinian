10a13310447be286c1a692ae63959d72232fc837
PurpleHato <linkvssangoku.jr@gmail.com>
2024-05-02 01:03:00 +0200

    TWEAK: Review suggestion with memcpy

--- mm/2s2h/Enhancements/Cycle/EndOfCycle.cpp
+++ mm/2s2h/Enhancements/Cycle/EndOfCycle.cpp
@@ -6,112 +6,65 @@ extern "C" {
 #include <overlays/kaleido_scope/ovl_kaleido_scope/z_kaleido_scope.h>
 }
 
-void RegisterEndOfCycleSaveHooks() {
-    static u8 tempConsumable;
-    static u8 tempRazorSword;
-    static u8 tempRupees;
-    static u8 tempItem;
-    static u8 tempAmmo;
-    static u8 tempItem2[255];
-    static u8 tempAmmo2[255];
-    static u8 tempBottle[6];
+SaveInfo saveInfoCopy;
 
-    GameInteractor::Instance->RegisterGameHook<GameInteractor::BeforeEndOfCycleSave>([]() {
+void RegisterEndOfCycleSaveHooks() {
+    
+    GameInteractor::Instance->RegisterGameHook<GameInteractor::BeforeEndOfCycleSave>([]() { 
+        memcpy(&saveInfoCopy, &gSaveContext.save.saveInfo, sizeof(SaveInfo));
+        });
+
+    GameInteractor::Instance->RegisterGameHook<GameInteractor::AfterEndOfCycleSave>([]() {
         if (CVarGetInteger("gEnhancements.Cycle.DoNotResetRupees", 0)) {
-            tempRupees = gSaveContext.save.saveInfo.playerData.rupees;
+            gSaveContext.save.saveInfo.playerData.rupees = saveInfoCopy.playerData.rupees;
         }
 
         if (CVarGetInteger("gEnhancements.Cycle.DoNotResetConsumables", 0)) {
             if (INV_CONTENT(ITEM_BOMB) == ITEM_BOMB) {
-                tempItem = INV_CONTENT(ITEM_BOMB);
-                tempAmmo = AMMO(tempItem);
+                INV_CONTENT(ITEM_BOMB) = saveInfoCopy.inventory.items[ITEM_BOMB];
+                AMMO(ITEM_BOMB) = saveInfoCopy.inventory.ammo[ITEM_BOMB];
             }
 
             if (INV_CONTENT(ITEM_DEKU_NUT) == ITEM_DEKU_NUT) {
-                tempItem = INV_CONTENT(ITEM_DEKU_NUT);
-                tempAmmo = AMMO(tempItem);
+                INV_CONTENT(ITEM_DEKU_NUT) = saveInfoCopy.inventory.items[ITEM_DEKU_NUT];
+                AMMO(ITEM_DEKU_NUT) = saveInfoCopy.inventory.ammo[ITEM_DEKU_NUT];
             }
 
             if (INV_CONTENT(ITEM_DEKU_STICK) == ITEM_DEKU_STICK) {
-                tempItem = INV_CONTENT(ITEM_DEKU_STICK);
-                tempAmmo = AMMO(tempItem);
+                INV_CONTENT(ITEM_DEKU_STICK) = saveInfoCopy.inventory.items[ITEM_DEKU_STICK];
+                AMMO(ITEM_DEKU_STICK) = saveInfoCopy.inventory.ammo[ITEM_DEKU_STICK];
             }
 
             if (INV_CONTENT(ITEM_BOW) == ITEM_BOW) {
-                tempItem = INV_CONTENT(ITEM_BOW);
-                tempAmmo = AMMO(tempItem);
+                INV_CONTENT(ITEM_BOW) = saveInfoCopy.inventory.items[ITEM_BOW];
+                AMMO(ITEM_BOW) = saveInfoCopy.inventory.ammo[ITEM_BOW];
             }
 
             for (int i = 0; i < ITEM_NUM_SLOTS; i++) {
                 if (gAmmoItems[i] != ITEM_NONE) {
                     if ((gSaveContext.save.saveInfo.inventory.items[i] != ITEM_NONE) && (i != SLOT_PICTOGRAPH_BOX)) {
-                        tempItem2[i] = gSaveContext.save.saveInfo.inventory.items[i];
-                        tempAmmo2[i] = gSaveContext.save.saveInfo.inventory.ammo[i];
+                        gSaveContext.save.saveInfo.inventory.items[i] = saveInfoCopy.inventory.items[i];
+                        gSaveContext.save.saveInfo.inventory.ammo[i] = saveInfoCopy.inventory.ammo[i];
                     }
                 }
             }
         }
-
+        
         if (CVarGetInteger("gEnhancements.Cycle.DoNotResetBottleContent", 0)) {
-            for (int i = SLOT_BOTTLE_1; i <= SLOT_BOTTLE_6; i++) {
-                tempBottle[i - SLOT_BOTTLE_1] = gSaveContext.save.saveInfo.inventory.items[i];
+            for (int i = ITEM_POTION_RED; i <= ITEM_OBABA_DRINK; i++) {
+                for (int j = EQUIP_SLOT_C_LEFT; j <= EQUIP_SLOT_C_RIGHT; j++) {
+                    if (GET_CUR_FORM_BTN_ITEM(j) == ITEM_BOTTLE) {
+                        SET_CUR_FORM_BTN_ITEM(j, saveInfoCopy.inventory.items[i]);
+                    }
+                }
+                gSaveContext.save.saveInfo.inventory.items[i] = saveInfoCopy.inventory.items[i];
             }
         }
 
         if (CVarGetInteger("gEnhancements.Cycle.DoNotResetRazorSword", 0) &&
-            GET_CUR_EQUIP_VALUE(EQUIP_TYPE_SWORD) == EQUIP_VALUE_SWORD_RAZOR) {
-            tempRazorSword = GET_CUR_EQUIP_VALUE(EQUIP_TYPE_SWORD) == EQUIP_VALUE_SWORD_RAZOR;
-        }
-    });
-
-    GameInteractor::Instance->RegisterGameHook<GameInteractor::AfterEndOfCycleSave>([]() {
-        if (CVarGetInteger("gEnhancements.Cycle.DoNotResetRupees", 0)) {
-            gSaveContext.save.saveInfo.playerData.rupees = tempRupees;
-        }
-
-        if (CVarGetInteger("gEnhancements.Cycle.DoNotResetConsumables", 0)) {
-            if (INV_CONTENT(ITEM_BOMB) == ITEM_BOMB) {
-                INV_CONTENT(ITEM_BOMB)  = tempItem;
-                AMMO(tempItem) = tempAmmo;
-            }
-
-            if (INV_CONTENT(ITEM_DEKU_NUT) == ITEM_DEKU_NUT) {
-                INV_CONTENT(ITEM_DEKU_NUT) = tempItem;
-                AMMO(tempItem) = tempAmmo;
-            }
-
-            if (INV_CONTENT(ITEM_DEKU_STICK) == ITEM_DEKU_STICK) {
-                INV_CONTENT(ITEM_DEKU_STICK) = tempItem;
-                AMMO(tempItem) = tempAmmo;
-            }
-
-            if (INV_CONTENT(ITEM_BOW) == ITEM_BOW) {
-                INV_CONTENT(ITEM_BOW) = tempItem;
-                AMMO(tempItem) = tempAmmo;
-            }
+            ((saveInfoCopy.equips.equipment & gEquipMasks[EQUIP_TYPE_SWORD]) >>
+             gEquipShifts[EQUIP_TYPE_SWORD]) == EQUIP_VALUE_SWORD_RAZOR) {
             
-            for (int i = 0; i < ITEM_NUM_SLOTS; i++) {
-                if (gAmmoItems[i] != ITEM_NONE) {
-                    if ((gSaveContext.save.saveInfo.inventory.items[i] != ITEM_NONE) && (i != SLOT_PICTOGRAPH_BOX)) {
-                        gSaveContext.save.saveInfo.inventory.items[i] = tempItem2[i];
-                        gSaveContext.save.saveInfo.inventory.ammo[i] = tempAmmo2[i];
-                    }
-                }
-            }
-        }
-
-        if (CVarGetInteger("gEnhancements.Cycle.DoNotResetBottleContent", 0)) {
-            for (int i = SLOT_BOTTLE_1; i <= SLOT_BOTTLE_6; i++) {
-                for (int j = EQUIP_SLOT_C_LEFT; j <= EQUIP_SLOT_C_RIGHT; j++) {
-                    if (GET_CUR_FORM_BTN_ITEM(j) == ITEM_BOTTLE) {
-                        SET_CUR_FORM_BTN_ITEM(j, tempBottle[i - SLOT_BOTTLE_1]);
-                    }
-                }
-                gSaveContext.save.saveInfo.inventory.items[i] = tempBottle[i - SLOT_BOTTLE_1];
-            }
-        }
-
-        if (CVarGetInteger("gEnhancements.Cycle.DoNotResetRazorSword", 0) && tempRazorSword) {
             SET_EQUIP_VALUE(EQUIP_TYPE_SWORD, EQUIP_VALUE_SWORD_RAZOR);
             CUR_FORM_EQUIP(EQUIP_SLOT_B) = ITEM_SWORD_RAZOR;
         }
