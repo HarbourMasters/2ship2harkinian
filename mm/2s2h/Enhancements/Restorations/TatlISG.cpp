e6901ab641069d1f0c1fa6e8bbb7166c6b3209dd
Patrick12115 <Patrick12115@hotmail.com>
2024-05-06 20:12:29 -0400

    Spelling error

--- mm/2s2h/Enhancements/Restorations/TatlISG.cpp
+++ mm/2s2h/Enhancements/Restorations/TatlISG.cpp
@@ -2,7 +2,7 @@
 #include "Enhancements/GameInteractor/GameInteractor.h"
 
 void RegisterTatlISG() {
-    REGISTER_VB_SHOULD(GI_VB_TATL_COVERSATION_AVAILABLE, {
+    REGISTER_VB_SHOULD(GI_VB_TATL_CONVERSATION_AVAILABLE, {
         if (CVarGetInteger("gEnhancements.Restorations.TatlISG", 0)) {
             *should = false;
         }
