dc3174ff28732de3919de73ebc6836116f86a32e
inspectredc <inspectredc@gmail.com>
2024-05-07 15:51:01 +0100

    remove redundant section in header

--- mm/2s2h/Enhancements/Camera/FreeLook.h
+++ mm/2s2h/Enhancements/Camera/FreeLook.h
@@ -3,13 +3,4 @@
 
 void RegisterCameraFreeLook();
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-#include <z64camera.h>
-bool Camera_FreeLook(Camera* camera);
-#ifdef __cplusplus
-}
-#endif
-
 #endif // FREE_LOOK_H
