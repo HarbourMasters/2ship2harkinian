20347ba150f683061647e672d5d31e261bb8acc6
Garrett Cox <garrettjcox@gmail.com>
2024-05-10 11:46:42 -0500

    Update mm/2s2h/resource/importer/TextureAnimationFactory.cpp
    Co-authored-by: Archez <Archez@users.noreply.github.com>

--- mm/2s2h/resource/importer/TextureAnimationFactory.cpp
+++ mm/2s2h/resource/importer/TextureAnimationFactory.cpp
@@ -2,9 +2,6 @@
 #include "2s2h/resource/type/TextureAnimation.h"
 #include <libultraship/libultraship.h>
 #include "spdlog/spdlog.h"
-#ifndef __APPLE__
-#include <malloc.h>
-#endif
 #include <stdlib.h>
 
 namespace SOH {
