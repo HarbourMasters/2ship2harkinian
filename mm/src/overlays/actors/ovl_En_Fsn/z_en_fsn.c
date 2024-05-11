0287fa2fefc60eb0495d7079b4c3d1ddb6f61118
louist103 <35883445+louist103@users.noreply.github.com>
2024-05-11 11:03:21 -0400

    Fix crash in en_fsn (#359)
    * Fix the bug maybe?
    
    * Comment
    
    * Add another fix
    
    * Another better fix
    
    * Fix again

--- mm/src/overlays/actors/ovl_En_Fsn/z_en_fsn.c
+++ mm/src/overlays/actors/ovl_En_Fsn/z_en_fsn.c
@@ -1480,8 +1480,12 @@ void EnFsn_Init(Actor* thisx, PlayState* play) {
     EnFsn* this = THIS;
 
     ActorShape_Init(&this->actor.shape, 0.0f, ActorShadow_DrawCircle, 20.0f);
+
+    // #region 2S2H [Port] ENFSN_LIMB_MAX originally had a +1 because the game incorrectly thought the skel had 18 limbs when it
+    // only has 17. We also need to patch the count in skelAnime because it is reading the wrong number from the skeleton header.
     SkelAnime_InitFlex(play, &this->skelAnime, &gFsnSkel, &gFsnIdleAnim, this->jointTable, this->morphTable,
                        ENFSN_LIMB_MAX);
+    this->skelAnime.limbCount = ENFSN_LIMB_MAX;
 
     if (ENFSN_IS_SHOP(&this->actor)) {
         this->actor.shape.rot.y = BINANG_ROT180(this->actor.shape.rot.y);
