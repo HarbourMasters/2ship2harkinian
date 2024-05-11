114fcced835c0c4779b2a53bf50b0977a0b3168a
louist103 <35883445+louist103@users.noreply.github.com>
2024-05-11 11:55:15 -0400

    Add comments in ossan.

--- mm/src/overlays/actors/ovl_En_Ossan/z_en_ossan.h
+++ mm/src/overlays/actors/ovl_En_Ossan/z_en_ossan.h
@@ -7,6 +7,8 @@
 #include "objects/object_fsn/object_fsn.h"
 
 // Note: adding 1 to FSN_LIMB_MAX due to bug in the skeleton, see bug in object_fsn.xml
+// #region [2S2H] Port FSN_LIMB_MAX is actually the number of limbs. The original game has a bug where it thinks it needs one more because
+// the skeleton is bugged. Same fix as en_fsn.
 #define ENOSSAN_LIMB_MAX MAX((s32)FSN_LIMB_MAX, (s32)ANI_LIMB_MAX)
 
 struct EnOssan;
