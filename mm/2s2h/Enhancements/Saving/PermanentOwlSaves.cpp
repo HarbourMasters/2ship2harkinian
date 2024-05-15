3432ba892eb4b959403f1aa616aae71990d4f41d
aMannus <mannusmenting@gmail.com>
2024-05-15 09:22:05 +0200

    Fix weirdness on Song of Time usage

--- mm/2s2h/Enhancements/Saving/PermanentOwlSaves.cpp
+++ mm/2s2h/Enhancements/Saving/PermanentOwlSaves.cpp
@@ -13,10 +13,25 @@ void RegisterPermanentOwlStatues() {
         }
     });
 
-    GameInteractor::Instance->RegisterGameHook<GameInteractor::AfterEndOfCycleSave>([]() {
+    GameInteractor::Instance->RegisterGameHook<GameInteractor::BeforeEndOfCycleSave>([]() {
         // Remove Owl Save on time cycle reset.
         if (CVarGetInteger("gEnhancements.Saving.PermanentOwlSaves", 0)) {
+
+            // Reset some gSaveContext stuff that usually happens when loading into a new savefile
+            // when there's an owl save present. Fixes some unexpected behaviour that otherwise happens.
+            if (gSaveContext.save.isFirstCycle) {
+                gSaveContext.save.entrance = ENTRANCE(SOUTH_CLOCK_TOWN, 0);
+                gSaveContext.save.day = 0;
+                gSaveContext.save.time = CLOCK_TIME(6, 0) - 1;
+            } else {
+                gSaveContext.save.entrance = ENTRANCE(CUTSCENE, 0);
+                gSaveContext.nextCutsceneIndex = 0;
+                gSaveContext.save.playerForm = PLAYER_FORM_HUMAN;
+            }
+
             func_80147314(&gPlayState->sramCtx, gSaveContext.fileNum);
+
+            gSaveContext.save.isOwlSave = 0;
         }
     });
 }
