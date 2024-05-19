5d08c9252cb7455f46a9c15c6053d09702525ddc
Adam Bird <archez39@me.com>
2024-05-18 22:43:11 -0400

    clean up masterquest copy paste

--- mm/2s2h/BenPort.h
+++ mm/2s2h/BenPort.h
@@ -64,10 +64,6 @@ void OTRGfxPrint(const char* str, void* printer, void (*printImpl)(void*, char))
 void OTRGetPixelDepthPrepare(float x, float y);
 uint16_t OTRGetPixelDepth(float x, float y);
 int32_t OTRGetLastScancode();
-uint32_t ResourceMgr_IsGameMasterQuest();
-uint32_t ResourceMgr_IsSceneMasterQuest(s16 sceneNum);
-uint32_t ResourceMgr_GameHasMasterQuest();
-uint32_t ResourceMgr_GameHasOriginal();
 uint32_t ResourceMgr_GetNumGameVersions();
 uint32_t ResourceMgr_GetGameVersion(int index);
 uint32_t ResourceMgr_GetGamePlatform(int index);
@@ -75,7 +71,6 @@ uint32_t ResourceMgr_GetGameRegion(int index);
 void ResourceMgr_LoadDirectory(const char* resName);
 char** ResourceMgr_ListFiles(const char* searchMask, int* resultSize);
 uint8_t ResourceMgr_FileExists(const char* resName);
-char* GetResourceDataByNameHandlingMQ(const char* path);
 void ResourceMgr_LoadFile(const char* resName);
 char* ResourceMgr_LoadFileFromDisk(const char* filePath);
 uint8_t ResourceMgr_ResourceIsBackground(char* texPath);
