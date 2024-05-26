3ca81dd758d4540b20d26462f2d121b6e2def319
Archez <Archez@users.noreply.github.com>
2024-05-26 17:09:31 -0400

    Rename CMake project from mm to 2ship (#487)

--- CMake/Packaging.cmake
+++ CMake/Packaging.cmake
@@ -76,9 +76,9 @@ execute_process(
     ${LINUXDEPLOY_EXECUTABLE}
     --appimage-extract-and-run
     --appdir=${CPACK_TEMPORARY_DIRECTORY}
-    --executable=$<TARGET_FILE:mm>
-    $<$<BOOL:$<TARGET_PROPERTY:mm,APPIMAGE_DESKTOP_FILE>>:--desktop-file=$<TARGET_PROPERTY:mm,APPIMAGE_DESKTOP_FILE>>
-    $<$<BOOL:$<TARGET_PROPERTY:mm,APPIMAGE_ICON_FILE>>:--icon-file=$<TARGET_PROPERTY:mm,APPIMAGE_ICON_FILE>>
+    --executable=$<TARGET_FILE:2ship>
+    $<$<BOOL:$<TARGET_PROPERTY:2ship,APPIMAGE_DESKTOP_FILE>>:--desktop-file=$<TARGET_PROPERTY:2ship,APPIMAGE_DESKTOP_FILE>>
+    $<$<BOOL:$<TARGET_PROPERTY:2ship,APPIMAGE_ICON_FILE>>:--icon-file=$<TARGET_PROPERTY:2ship,APPIMAGE_ICON_FILE>>
     --output=appimage
     # --verbosity=2
 )
