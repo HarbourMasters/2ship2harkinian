81c8c2ae8e2c379171689a62d54c711203241eeb
Caladius <Caladius@users.noreply.github.com>
2024-05-29 19:11:11 -0400

    Updated to reflect Safety nets for item duplication, also cleaned up code to allow for expansion later.

--- mm/2s2h/Enhancements/Cheats/Infinite.cpp
+++ mm/2s2h/Enhancements/Cheats/Infinite.cpp
@@ -26,17 +26,23 @@ void RegisterInfiniteCheats() {
         }
 
         if (CVarGetInteger("gCheats.InfiniteConsumables", 0)) {
-            AMMO(ITEM_BOW) = CUR_CAPACITY(UPG_QUIVER);
-            AMMO(ITEM_BOMB) = CUR_CAPACITY(UPG_BOMB_BAG);
-            AMMO(ITEM_BOMBCHU) = CUR_CAPACITY(UPG_BOMB_BAG);
-            AMMO(ITEM_DEKU_STICK) = CUR_CAPACITY(UPG_DEKU_STICKS);
-            AMMO(ITEM_DEKU_NUT) = CUR_CAPACITY(UPG_DEKU_NUTS);
-            AMMO(ITEM_MAGIC_BEANS) = 20;
-            if (INV_CONTENT(ITEM_POWDER_KEG) != ITEM_NONE) {
-                AMMO(ITEM_POWDER_KEG) = 1;
-            } else {
-                AMMO(ITEM_POWDER_KEG) = 0;
-            }
+            uint32_t ammoItems[7] = {   ITEM_BOW,         ITEM_BOMB,          ITEM_BOMBCHU,   ITEM_DEKU_STICK,
+                                        ITEM_DEKU_NUT,    ITEM_MAGIC_BEANS,   ITEM_POWDER_KEG };
+            uint32_t upgCapacity[7] = { UPG_QUIVER,     UPG_BOMB_BAG,       UPG_BOMB_BAG,   UPG_DEKU_STICKS,
+                                        UPG_DEKU_NUTS,  ITEM_MAGIC_BEANS,   ITEM_POWDER_KEG };
+            for (int i = 0; i <= sizeof(ammoItems) / sizeof(ammoItems[0]); i++) {
+                if (INV_CONTENT(ammoItems[i]) == ammoItems[i]) {
+                    if (ammoItems[i] == ITEM_MAGIC_BEANS) {
+                        AMMO(ammoItems[i]) = 20;
+                    } else if (ammoItems[i] == ITEM_POWDER_KEG) {
+                        AMMO(ammoItems[i]) = 1;
+                    } else {
+                        AMMO(ammoItems[i]) = CUR_CAPACITY(upgCapacity[i]);
+                    }
+                } else if (ammoItems[i] == ITEM_POWDER_KEG) {
+                    AMMO(ammoItems[i]) = 0;
+                }
+            };
         }
     });
 }
