57d6e2125204a0b606e34c66195b2c964dde3dc0
unknown <mothstery@gmail.com>
2024-05-31 17:04:30 -0700

    fix edge case of marine researcher dialogue being wrong with low egg counts

--- mm/2s2h/Enhancements/Songs/ZoraEggCount.cpp
+++ mm/2s2h/Enhancements/Songs/ZoraEggCount.cpp
@@ -1,6 +1,7 @@
 #include "ZoraEggCount.h"
 #include <libultraship/bridge.h>
 #include "Enhancements/GameInteractor/GameInteractor.h"
+#include "global.h"
 
 void RegisterZoraEggCount() {
     REGISTER_VB_SHOULD(GI_VB_SET_ZORA_EGG_COUNT, {
@@ -9,6 +10,11 @@ void RegisterZoraEggCount() {
             CVarSetInteger("gEnhancements.Songs.ZoraEggCount", 7);
         }
         s32* count = static_cast<int*>(opt);
-        *count = CVarGetInteger("gEnhancements.Songs.ZoraEggCount", 7);
+        // Ignore variable count if you have no eggs, fixes dialogue with marine researcher.
+        if ((gSaveContext.save.saveInfo.permanentSceneFlags[SCENE_LABO].unk_14 & 7) <= 0) {
+            *count = 7;
+        } else {
+            *count = CVarGetInteger("gEnhancements.Songs.ZoraEggCount", 7);
+        }
     });
 }
