30242b11e1c2f36b1c7978ba81ffcd303b555401
Caladius <Caladius@users.noreply.github.com>
2024-06-01 23:25:07 -0400

    Alphabetical Menu Listing + clang

--- mm/2s2h/BenGui/BenMenuBar.cpp
+++ mm/2s2h/BenGui/BenMenuBar.cpp
@@ -521,11 +521,11 @@ void DrawEnhancementsMenu() {
         }
 
         if (UIWidgets::BeginMenu("Masks")) {
+            UIWidgets::CVarCheckbox("Blast Mask has Powder Keg Force", "gEnhancements.Masks.BlastMaskKeg", {});
             UIWidgets::CVarCheckbox("Fast Transformation", "gEnhancements.Masks.FastTransformation");
             UIWidgets::CVarCheckbox("Fierce Deity's Mask Anywhere", "gEnhancements.Masks.FierceDeitysAnywhere",
                                     { .tooltip = "Allow using Fierce Deity's mask outside of boss rooms." });
             UIWidgets::CVarCheckbox("No Blast Mask Cooldown", "gEnhancements.Masks.NoBlastMaskCooldown", {});
-            UIWidgets::CVarCheckbox("Blast Mask has Powder Keg Force", "gEnhancements.Masks.BlastMaskKeg", {});
 
             ImGui::EndMenu();
         }
--- mm/2s2h/Enhancements/Masks/BlastMaskKeg.cpp
+++ mm/2s2h/Enhancements/Masks/BlastMaskKeg.cpp
@@ -10,22 +10,22 @@ EquipSlot func_8082FDC4(void);
 }
 
 void RegisterBlastMaskKeg() {
-    GameInteractor::Instance->RegisterGameHookForID<GameInteractor::ShouldActorInit>(ACTOR_EN_BOM, [](Actor* actor, bool* should) {
-        Player* player = GET_PLAYER(gPlayState);
-        if (CVarGetInteger("gEnhancements.Masks.BlastMaskKeg", 0)) {
-            ItemId item;
-            EquipSlot i = func_8082FDC4();
+    GameInteractor::Instance->RegisterGameHookForID<GameInteractor::ShouldActorInit>(
+        ACTOR_EN_BOM, [](Actor* actor, bool* should) {
+            Player* player = GET_PLAYER(gPlayState);
+            if (CVarGetInteger("gEnhancements.Masks.BlastMaskKeg", 0)) {
+                ItemId item;
+                EquipSlot i = func_8082FDC4();
 
-            i = ((i >= EQUIP_SLOT_A) && (player->transformation == PLAYER_FORM_FIERCE_DEITY) &&
-                 (player->heldItemAction != PLAYER_IA_SWORD_TWO_HANDED))
-                    ? EQUIP_SLOT_B
-                    : i;
+                i = ((i >= EQUIP_SLOT_A) && (player->transformation == PLAYER_FORM_FIERCE_DEITY) &&
+                     (player->heldItemAction != PLAYER_IA_SWORD_TWO_HANDED))
+                        ? EQUIP_SLOT_B
+                        : i;
 
-            item = Player_GetItemOnButton(gPlayState, player, i);
-            if (item == ITEM_F0) {
-                actor->shape.rot.x = BOMB_EXPLOSIVE_TYPE_POWDER_KEG;
+                item = Player_GetItemOnButton(gPlayState, player, i);
+                if (item == ITEM_F0) {
+                    actor->shape.rot.x = BOMB_EXPLOSIVE_TYPE_POWDER_KEG;
+                }
             }
-        }
-	});
-	
+        });
 }
