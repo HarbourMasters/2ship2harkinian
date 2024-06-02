c0d461758d80ef49efc233cc5b16e8c56084c963
Caladius <Caladius@users.noreply.github.com>
2024-06-01 21:00:41 -0400

    Update to HOOK_ID and adds Withdrawal functionaility.

--- mm/2s2h/Enhancements/Actor/BankerDialogue.cpp
+++ mm/2s2h/Enhancements/Actor/BankerDialogue.cpp
@@ -5,11 +5,24 @@
 
 #include "overlays/actors/ovl_En_Ginko_Man/z_en_ginko_man.h"
 
+static const char zeroRupees[3] = { '0', '0', '0' };
+
+
+void BankerDialogue_UpdateMessage(const char rupeeValue[3]) {
+    for (int i = 0; i <= 2; i++) {
+        gPlayState->msgCtx.decodedBuffer.schar[gPlayState->msgCtx.unk120C0 + i] = rupeeValue[i];
+        Font_LoadCharNES(gPlayState, gPlayState->msgCtx.decodedBuffer.schar[gPlayState->msgCtx.unk120C0 + i],
+                         gPlayState->msgCtx.unk120C4 + (i << 7));
+    }
+    Audio_PlaySfx(NA_SE_SY_RUPY_COUNT);
+}
+
 void RegisterBankerDialogue() {
     GameInteractor::Instance->RegisterGameHookForID<GameInteractor::OnActorInit>(
         ACTOR_EN_GINKO_MAN, [](Actor* outerActor) {
-            static uint32_t enGinkoUpdateHook = 0;
-            static uint32_t enGinkoKillHook = 0;
+            
+            static HOOK_ID enGinkoUpdateHook = 0;
+            static HOOK_ID enGinkoKillHook = 0;
             GameInteractor::Instance->UnregisterGameHookForPtr<GameInteractor::OnActorUpdate>(enGinkoUpdateHook);
             GameInteractor::Instance->UnregisterGameHook<GameInteractor::OnSceneInit>(enGinkoKillHook);
             enGinkoUpdateHook = 0;
@@ -27,26 +40,39 @@ void RegisterBankerDialogue() {
                                 char thirdChar = (gSaveContext.save.saveInfo.playerData.rupees % 10) + '0';
                                 const char rupeeChar[3] = { firstChar, secondChar, thirdChar };
 
-                                for (int i = 0; i <= 2; i++) {
-                                    gPlayState->msgCtx.decodedBuffer.schar[gPlayState->msgCtx.unk120C0 + i] =
-                                        rupeeChar[i];
-                                    Font_LoadCharNES(
-                                        gPlayState,
-                                        gPlayState->msgCtx.decodedBuffer.schar[gPlayState->msgCtx.unk120C0 + i],
-                                        gPlayState->msgCtx.unk120C4 + (i << 7));
-                                }
-                                Audio_PlaySfx(NA_SE_SY_RUPY_COUNT);
+                                BankerDialogue_UpdateMessage(rupeeChar);
                             }
+                        } else if (gPlayState->msgCtx.currentTextId == 1134 ) {
+                            uint32_t walletSize = CUR_UPG_VALUE(UPG_WALLET);
+                            uint32_t currentRupees = gSaveContext.save.saveInfo.playerData.rupees;
+                            uint32_t maxWallet;
+                            if (CHECK_BTN_ALL(gPlayState->state.input[0].cur.button, BTN_Z)) {
+                                switch (ITEM_WALLET_DEFAULT + walletSize) {
+                                    case ITEM_WALLET_ADULT:
+                                        maxWallet = (200 - currentRupees);
+                                        break;
+                                    case ITEM_WALLET_GIANT:
+                                        maxWallet = (500 - currentRupees);
+                                        break;
+                                    default:
+                                        maxWallet = (99 - currentRupees);
+                                        break;
+                                }
+                                if (maxWallet != 0 && gPlayState->msgCtx.bankRupeesSelected != maxWallet) {
+                                    char firstChar = (maxWallet / 100) + '0';
+                                    char secondChar = ((maxWallet / 10) % 10) + '0';
+                                    char thirdChar = (maxWallet % 10) + '0';
+                                    const char rupeeChar[3] = { firstChar, secondChar, thirdChar };
+
+                                    BankerDialogue_UpdateMessage(rupeeChar);
+                                }
+                            }
+                        }
+                        if (gPlayState->msgCtx.currentTextId == 1104 || gPlayState->msgCtx.currentTextId == 1134) {
                             if (CHECK_BTN_ALL(gPlayState->state.input[0].cur.button, BTN_R) &&
                                 gPlayState->msgCtx.bankRupeesSelected != 0) {
-                                for (int i = 0; i <= 2; i++) {
-                                    gPlayState->msgCtx.decodedBuffer.schar[gPlayState->msgCtx.unk120C0 + i] = '0';
-                                    Font_LoadCharNES(
-                                        gPlayState,
-                                        gPlayState->msgCtx.decodedBuffer.schar[gPlayState->msgCtx.unk120C0 + i],
-                                        gPlayState->msgCtx.unk120C4 + (i << 7));
-                                }
-                                Audio_PlaySfx(NA_SE_SY_RUPY_COUNT);
+
+                                BankerDialogue_UpdateMessage(zeroRupees);
                             }
                         }
                     }
