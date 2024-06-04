42bf45c570ccf875ee53ee2677926e9582c85f91
Garrett Cox <garrettjcox@gmail.com>
2024-06-03 23:52:51 -0500

    Update mm/2s2h/Enhancements/Cheats/LongerFlowerGlide.cpp

--- mm/2s2h/Enhancements/Cheats/LongerFlowerGlide.cpp
+++ mm/2s2h/Enhancements/Cheats/LongerFlowerGlide.cpp
@@ -7,8 +7,8 @@ extern f32 D_8085D958[2];
 
 void RegisterLongerFlowerGlide() {
     if (CVarGetInteger("gCheats.LongerFlowerGlide", 0)) {
-        D_8085D958[0] = MAXFLOAT;
-        D_8085D958[1] = MAXFLOAT;
+        D_8085D958[0] = 99999.9f;
+        D_8085D958[1] = 99999.9f;
     } else {
         D_8085D958[0] = 600.0f;
         D_8085D958[1] = 960.0f;
