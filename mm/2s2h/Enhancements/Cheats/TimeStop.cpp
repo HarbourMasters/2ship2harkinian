cf4fa6869ded81070954bbef62754640428ddec5
Travis Smith <balloondude2@gmail.com>
2024-06-20 22:15:51 -0600

    clean up

--- mm/2s2h/Enhancements/Cheats/TimeStop.cpp
+++ mm/2s2h/Enhancements/Cheats/TimeStop.cpp
@@ -60,28 +60,5 @@ void RegisterTimeStopInTemples() {
             default:
                 break;
         }
-
-        if (CVarGetInteger("gCheats.TempleTimeStop", 0)) {
-
-            switch (gPlayState->sceneId) {
-                // Woodfall Temple + Odolwa
-                case SCENE_MITURIN:
-                case SCENE_MITURIN_BS:
-                // Snowhead Temple + Goht
-                case SCENE_HAKUGIN:
-                case SCENE_HAKUGIN_BS:
-                // Great Bay Temple + Gyorg
-                case SCENE_SEA:
-                case SCENE_SEA_BS:
-                // Stone Tower Temple (+ inverted) + Twinmold
-                case SCENE_INISIE_N:
-                case SCENE_INISIE_R:
-                case SCENE_INISIE_BS:
-                    R_TIME_SPEED = 0;
-                    break;
-                default:
-                    break;
-            }
-        }
     });
 }
