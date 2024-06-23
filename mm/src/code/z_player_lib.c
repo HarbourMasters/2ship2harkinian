a933d73589a61a0d17abba1d7222db0825ef0ab2
louist103 <35883445+louist103@users.noreply.github.com>
2024-06-23 17:11:21 -0400

    Fix the check for preventing a HESS crash. (#710)
    * Fix the check
    
    * Change style
    
    * Another fix in SKelanime.
    
    * Format

--- mm/src/code/z_player_lib.c
+++ mm/src/code/z_player_lib.c
@@ -2415,7 +2415,7 @@ s32 Player_OverrideLimbDrawGameplayDefault(PlayState* play, s32 limbIndex, Gfx**
                     if (handIndex != 0) {
                         handIndex = (handIndex >> 8) - 1;
                         if (CVarGetInteger("gEnhancements.Fixes.HessCrash", 1)) {
-                            if (handIndex > 2) {
+                            if (handIndex >= 2) {
                                 handIndex = 0;
                             }
                         }
