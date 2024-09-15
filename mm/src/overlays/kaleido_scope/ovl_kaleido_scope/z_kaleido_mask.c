755455fa4a86e6558f0aa73a8170beba1d528d40
mckinlee <mckinlee@ymail.com>
2024-09-15 16:53:34 -0400

    support persistent bunny hood, utilize GI_VB_KALEIDO_DISPLAY_ITEM_TEXT

--- mm/src/overlays/kaleido_scope/ovl_kaleido_scope/z_kaleido_mask.c
+++ mm/src/overlays/kaleido_scope/ovl_kaleido_scope/z_kaleido_mask.c
@@ -10,10 +10,6 @@
 #include "BenGui/HudEditor.h"
 #include "2s2h/Enhancements/GameInteractor/GameInteractor.h"
 
-#include "macros.h"
-
-#include "2s2h/Enhancements/Masks/EasyMaskEquip.h"
-
 s16 sMaskEquipState = EQUIP_STATE_MAGIC_ARROW_GROW_ORB;
 
 // Timer to hold magic arrow icon over magic arrow slot before moving when equipping.
@@ -674,12 +670,9 @@ void KaleidoScope_UpdateMaskCursor(PlayState* play) {
                     sMaskEquipState = EQUIP_STATE_MOVE_TO_C_BTN;
                     sMaskEquipAnimTimer = 10;
                     Audio_PlaySfx(NA_SE_SY_DECIDE);
-                    // EasyMaskEquip check disables the mask description textbox from appearing when a mask is selected
-                    // with A button.
                 } else if ((pauseCtx->debugEditor == DEBUG_EDITOR_NONE) && (pauseCtx->state == PAUSE_STATE_MAIN) &&
                            (pauseCtx->mainState == PAUSE_MAIN_STATE_IDLE) &&
-                           CHECK_BTN_ALL(input->press.button, BTN_A) && (msgCtx->msgLength == 0) &&
-                           !EasyMaskEquip_IsEnabled()) {
+                           CHECK_BTN_ALL(input->press.button, BTN_A) && (msgCtx->msgLength == 0)) {
                     if (GameInteractor_Should(GI_VB_KALEIDO_DISPLAY_ITEM_TEXT, true,
                                               &pauseCtx->cursorItem[PAUSE_MASK])) {
                         // Give description on item through a message box
