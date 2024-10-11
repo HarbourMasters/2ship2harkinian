caea52257029683191453b612c28c3e217d67cce
cplaster <nixhex@gmail.com>
2024-10-11 14:14:29 -0500

    Updated per garrettjoecox suggestions

--- mm/2s2h/Enhancements/Cheats/HookshotAnywhere.cpp
+++ mm/2s2h/Enhancements/Cheats/HookshotAnywhere.cpp
@@ -3,7 +3,7 @@
 #include "Enhancements/GameInteractor/GameInteractor.h"
 
 void RegisterHookshotAnywhere() {
-    REGISTER_VB_SHOULD(GI_VB_ENABLE_HOOKSHOT_ANYWHERE, {
+    REGISTER_VB_SHOULD(GI_VB_BE_HOOKSHOT_SURFACE, {
         if (CVarGetInteger("gCheats.HookshotAnywhere", 0)) {
             *should = true;
         }
