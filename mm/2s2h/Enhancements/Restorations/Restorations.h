817907d9948297b8d6fbcc56f4cf7382c23dcba1
Garrett Cox <garrettjcox@gmail.com>
2024-10-11 11:27:38 -0500

    Back-port GC credits easter egg with our own twist

--- mm/2s2h/Enhancements/Restorations/Restorations.h
+++ mm/2s2h/Enhancements/Restorations/Restorations.h
@@ -2,6 +2,7 @@
 #define ENHANCEMENTS_RESTORATIONS_H
 
 void RegisterVariableFlipHop();
+void RegisterHiddenCredits();
 void RegisterPowerCrouchStab();
 void RegisterSideRoll();
 void RegisterTatlISG();
