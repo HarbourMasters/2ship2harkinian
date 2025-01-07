925b62f9f1424b51b8faae614c3568ae0a153614
Garrett Cox <garrettjcox@gmail.com>
2025-01-07 09:53:44 -0600

    Refactor the monkey skip to fix some issues

--- mm/2s2h/Enhancements/Cutscenes/MiscInteractions/SkipBothersomeMonkey.cpp
+++ mm/2s2h/Enhancements/Cutscenes/MiscInteractions/SkipBothersomeMonkey.cpp
@@ -6,32 +6,18 @@ extern "C" {
 #include "variables.h"
 #include "functions.h"
 #include "src/overlays/actors/ovl_En_Mnk/z_en_mnk.h"
-void EnMnk_Monkey_SetupRunAfterTalk(EnMnk* thisx, PlayState* play);
-void EnMnk_Monkey_ApproachPlayer(EnMnk* thisx, PlayState* play);
-void EnMnk_Monkey_WaitOutsideWoods(EnMnk* thisx, PlayState* play);
+void EnMnk_Monkey_WaitToRun(EnMnk* thisx, PlayState* play);
 }
 
 #define CVAR_NAME "gEnhancements.Cutscenes.SkipMiscInteractions"
 #define CVAR CVarGetInteger(CVAR_NAME, 0)
 
 void RegisterSkipBothersomeMonkey() {
-    COND_ID_HOOK(OnActorUpdate, ACTOR_EN_MNK, CVAR, [](Actor* actor) {
-        if (MONKEY_GET_TYPE(actor) == MONKEY_OUTSIDEWOODS) {
-            EnMnk* monkey = (EnMnk*)actor;
-            PlayState* play = gPlayState;
-
-            if (monkey->actionFunc == EnMnk_Monkey_WaitOutsideWoods) {
-                // Trigger monkey approach
-                SET_EVENTINF(EVENTINF_26);
-                EnMnk_Monkey_ApproachPlayer(monkey, play);
-            } else if (monkey->actionFunc == EnMnk_Monkey_ApproachPlayer) {
-                // Skip dialogue and make monkey run
-                SET_EVENTINF(EVENTINF_25);
-                SET_WEEKEVENTREG(WEEKEVENTREG_79_02);
-                EnMnk_Monkey_SetupRunAfterTalk(monkey, play);
-                Flags_SetSwitch(play, MONKEY_GET_SWITCH_FLAG(&monkey->picto.actor));
-            }
-        }
+    COND_VB_SHOULD(VB_MONKEY_WAIT_TO_TALK_AFTER_APPROACH, CVAR, {
+        EnMnk* monkey = (EnMnk*)va_arg(args, EnMnk*);
+        *should = false;
+        SET_EVENTINF(EVENTINF_25);
+        monkey->actionFunc = EnMnk_Monkey_WaitToRun;
     });
 }
 
