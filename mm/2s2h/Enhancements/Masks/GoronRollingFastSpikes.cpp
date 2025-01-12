2cf001b469615a888ae27f171597a8283ffa4022
aMannus <mannusmenting@gmail.com>
2025-01-12 13:45:01 +0100

    Clang pls

--- mm/2s2h/Enhancements/Masks/GoronRollingFastSpikes.cpp
+++ mm/2s2h/Enhancements/Masks/GoronRollingFastSpikes.cpp
@@ -11,13 +11,13 @@ extern PlayState* gPlayState;
 }
 
 void RegisterGoronRollingFastSpikes() {
-    COND_VB_SHOULD(VB_GORON_ROLL_INCREASE_SPIKE_LEVEL, CVAR, { 
+    COND_VB_SHOULD(VB_GORON_ROLL_INCREASE_SPIKE_LEVEL, CVAR, {
         Player* player = GET_PLAYER(gPlayState);
 
         // This is what the game keeps track of the state of how far you're off to spikes.
         // The vanilla if statement this SHOULD comes from will set it back to 4 if the player
         // doesn't have magic, so it doesn't conflict with that or "Goron Rolling Ignores Magic".
-        // 
+        //
         // Vanilla checks for number 54 for spike mode, but still having a slight wind-up looks
         // and feels much better.
         player->av1.actionVar1 += 8;
--- mm/2s2h/Enhancements/Masks/GoronRollingIgnoresMagic.cpp
+++ mm/2s2h/Enhancements/Masks/GoronRollingIgnoresMagic.cpp
@@ -22,8 +22,8 @@ void RegisterGoronRollingIgnoresMagic() {
     // Mimicking the vanilla condition minus the magic check
     COND_VB_SHOULD(VB_GORON_ROLL_DISABLE_SPIKE_MODE, CVAR, {
         Player* player = GET_PLAYER(gPlayState);
-        bool disableSpikes =
-            (player->stateFlags3 & PLAYER_STATE3_80000) && (!CHECK_BTN_ALL(sPlayerControlInput->cur.button, BTN_A) ||
+        bool disableSpikes = (player->stateFlags3 & PLAYER_STATE3_80000) &&
+                             (!CHECK_BTN_ALL(sPlayerControlInput->cur.button, BTN_A) ||
                               ((player->av1.actionVar1 == 4) && (player->unk_B08 < 12.0f)));
         if (!disableSpikes) {
             *should = false;
