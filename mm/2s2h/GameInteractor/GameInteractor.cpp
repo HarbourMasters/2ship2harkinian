f310e42f0c697a8f08f1f3e99c6c2a95aebf06fa
lightmanLP <liteman1000@gmail.com>
2025-02-06 17:47:56 +0700

    shield invert cvars
    idk why there was no any before

--- mm/2s2h/GameInteractor/GameInteractor.cpp
+++ mm/2s2h/GameInteractor/GameInteractor.cpp
@@ -369,8 +369,6 @@ int GameInteractor_InvertControl(GIInvertType type) {
                 result *= -1;
             }
             break;
-        case GI_INVERT_SHIELD_Y:
-            if (CVarGetInteger("gEnhancements.Equipment.InvertShieldY", 0)) {
         case GI_INVERT_FIRST_PERSON_MOUSE_X:
             if (CVarGetInteger("gEnhancements.Camera.Mouse.FirstPerson.InvertX", 0)) {
                 result *= -1;
@@ -383,6 +381,16 @@ int GameInteractor_InvertControl(GIInvertType type) {
             break;
         case GI_INVERT_ZORA_SWIM_Y:
             if (CVarGetInteger("gEnhancements.Player.InvertZoraSwimY", 1)) {
+                result *= -1;
+            }
+            break;
+        case GI_INVERT_SHIELD_X:
+            if (CVarGetInteger("gEnhancements.Equipment.InvertShieldX", 0)) {
+                result *= -1;
+            }
+            break;
+        case GI_INVERT_SHIELD_Y:
+            if (CVarGetInteger("gEnhancements.Equipment.InvertShieldY", 1)) {
                 result *= -1;
             }
             break;
