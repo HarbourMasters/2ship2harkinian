ada0475ddd0225ab391f00040e436070a1b189da
Caladius <Caladius@users.noreply.github.com>
2025-02-16 19:21:22 -0500

    Song Icon skew

--- mm/2s2h/Rando/Menu.cpp
+++ mm/2s2h/Rando/Menu.cpp
@@ -314,7 +314,9 @@ static void DrawItemsTab() {
             ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(1.0f, 1.0f, 1.0f, 0.0f));
             ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(1.0f, 1.0f, 1.0f, 0.2f));
             ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(1.0f, 1.0f, 1.0f, 0.1f));
-            if (ImGui::ImageButton(std::to_string(i).c_str(), textureId, ImVec2(columnWidth / 8, columnWidth / 8),
+            if (ImGui::ImageButton(std::to_string(i).c_str(), textureId,
+                                   i >= 65 ? ImVec2(columnWidth / 10, columnWidth / 8)
+                                           : ImVec2(columnWidth / 8, columnWidth / 8),
                                    ImVec2(0, 0), ImVec2(1, 1), ImVec4(0, 0, 0, 0), iconColor((int16_t)itemId))) {
                 startingItems |= (1 << i);
                 CVarSetInteger(Rando::StaticData::Options[optionId].cvar, startingItems);
