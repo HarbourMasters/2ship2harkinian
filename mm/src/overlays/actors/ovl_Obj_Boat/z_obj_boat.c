3dde8bbe3b8295a8fe9b3eb97cf93a7299f63f36
louist103 <35883445+louist103@users.noreply.github.com>
2025-03-03 03:42:09 +0000

    Format

--- mm/src/overlays/actors/ovl_Obj_Boat/z_obj_boat.c
+++ mm/src/overlays/actors/ovl_Obj_Boat/z_obj_boat.c
@@ -162,8 +162,8 @@ void ObjBoat_UpdateCutscene(Actor* thisx, PlayState* play2) {
                 if (cue->id == 3) {
                     path = &play->setupPathList[path->additionalPathIndex];
                 }
-                //! @bug This is missing a -1 for last valid index  
-                // 2S2H [Port] Opting to fix this to keep it in bounds 
+                //! @bug This is missing a -1 for last valid index
+                // 2S2H [Port] Opting to fix this to keep it in bounds
                 this->maxPointIndex = path->count - 1;
                 this->points = Lib_SegmentedToVirtual(path->points);
                 Math_Vec3s_ToVec3f(&this->dyna.actor.world.pos, this->points);
