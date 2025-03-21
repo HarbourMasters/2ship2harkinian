e5a30591c6fb81bc8d0dc6c357a312f64cd661dd
Extloga <141232749+Extloga@users.noreply.github.com>
2025-03-21 22:39:19 +0100

    Fix for the English text in BenMenu.cpp

--- mm/2s2h/BenGui/BenMenu.cpp
+++ mm/2s2h/BenGui/BenMenu.cpp
@@ -1318,11 +1318,10 @@ void BenMenu::AddEnhancements() {
         .CVar("gEnhancements.DifficultyOptions.GibdoTradeSequence")
         .Options(
             ComboboxOptions()
-                .Tooltip(
-                    "Changes the way the Gibdo Trade Sequence works\n"
+                .Tooltip("Changes the way the Gibdo Trade Sequence works:\n"
                     "- Vanilla: Works normally.\n"
                     "- MM3D: Gibdos will only take one quantity of the item they request, as they do in MM3D. "
-                    "  The Gibdo requesting a blue potion will also accept a red potion.\n"
+                    "The Gibdo requesting a blue potion will also accept a red potion.\n"
                     "- No trade: Gibdos will vanish without taking items.")
                 .DefaultIndex(GibdoTradeSequenceOptions::GIBDO_TRADE_SEQUENCE_VANILLA)
                 .ComboMap(gibdoTradeSequenceOptions));
