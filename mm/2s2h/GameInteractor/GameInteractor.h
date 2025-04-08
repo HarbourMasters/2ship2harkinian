9910e146b26d453cd464062cc1e81f9ee26545f8
Rachael LaMaster <rachael.lamaster@gmail.com>
2025-04-08 12:44:16 -0400

    Added a trailing comma to the new vanilla behavior

--- mm/2s2h/GameInteractor/GameInteractor.h
+++ mm/2s2h/GameInteractor/GameInteractor.h
@@ -236,7 +236,7 @@ typedef enum {
     VB_SNOWBALL_DROP_COLLECTIBLE,
     VB_SNOWBALL_SET_FLAG,
     VB_START_JUMPSLASH,
-    VB_SETUP_TRANSITION
+    VB_SETUP_TRANSITION,
 } GIVanillaBehavior;
 
 typedef enum {
