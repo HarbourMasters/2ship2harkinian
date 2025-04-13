bfe1a2b0139076807916113d60bbb16dd52a2335
louist103 <35883445+louist103@users.noreply.github.com>
2025-04-12 22:27:06 -0400

    Bump extraction tools and bring over OOT changes

--- CMake/copy-existing-otrs.cmake
+++ CMake/copy-existing-otrs.cmake
@@ -1,12 +1,12 @@
 message(STATUS "Copying otr files...")
 
 # Copy and rename mm.zip to mm.o2r
-if(NOT ONLY2SHIPOTR AND EXISTS ${SOURCE_DIR}/OTRExporter/mm.zip)
-    execute_process(COMMAND ${CMAKE_COMMAND} -E copy mm.zip ${SOURCE_DIR}/mm.o2r)
-    execute_process(COMMAND ${CMAKE_COMMAND} -E copy mm.zip ${BINARY_DIR}/mm/mm.o2r)
+if(NOT ONLY2SHIPOTR AND EXISTS ${SOURCE_DIR}/mm/mm.o2r)
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy mm.o2r ${SOURCE_DIR}/mm.o2r)
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy mm.o2r ${BINARY_DIR}/mm/mm.o2r)
     message(STATUS "Copied mm.zip")
 endif()
-if(EXISTS ${SOURCE_DIR}/OTRExporter/2ship.o2r)
+if(EXISTS ${SOURCE_DIR}/mm/2ship.o2r)
     execute_process(COMMAND ${CMAKE_COMMAND} -E copy 2ship.o2r ${SOURCE_DIR})
     execute_process(COMMAND ${CMAKE_COMMAND} -E copy 2ship.o2r ${BINARY_DIR}/mm/)
     message(STATUS "Copied 2ship.o2r")
@@ -14,8 +14,8 @@ endif()
 
 # Additionally for Windows, copy the otrs to the target dir, side by side with 2ship.exe
 if(SYSTEM_NAME MATCHES "Windows")
-    if(NOT ONLY2SHIPOTR AND EXISTS ${SOURCE_DIR}/OTRExporter/mm.zip)
-        execute_process(COMMAND ${CMAKE_COMMAND} -E copy mm.zip ${TARGET_DIR}/mm.o2r)
+    if(NOT ONLY2SHIPOTR AND EXISTS ${SOURCE_DIR}/mm/mm.o2r)
+        execute_process(COMMAND ${CMAKE_COMMAND} -E copy mm.o2r ${TARGET_DIR}/mm.o2r)
     endif()
     if(EXISTS ${SOURCE_DIR}/OTRExporter/2ship.o2r)
         execute_process(COMMAND ${CMAKE_COMMAND} -E copy 2ship.o2r ${TARGET_DIR})
