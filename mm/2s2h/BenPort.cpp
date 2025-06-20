12a08f8ecbff5ee268677c10ad3b47d1b0dc8553
louist103 <35883445+louist103@users.noreply.github.com>
2025-06-20 11:15:38 -0400

    Font fix

--- mm/2s2h/BenPort.cpp
+++ mm/2s2h/BenPort.cpp
@@ -345,7 +345,12 @@ ImFont* OTRGlobals::CreateFontWithSize(float size, std::string fontPath) {
         initData->Path = fontPath;
         std::shared_ptr<Ship::Font> fontData = std::static_pointer_cast<Ship::Font>(
             Ship::Context::GetInstance()->GetResourceManager()->LoadResource(fontPath, false, initData));
-        font = mImGuiIo->Fonts->AddFontFromMemoryTTF(fontData->Data, fontData->DataSize, size);
+        void* fontDataCpy = malloc(fontData->DataSize);
+        // ImGui will free this memory when it closes. We must copy it into our own buffer to prevent a double free.
+        if (fontDataCpy != nullptr) {
+            memcpy(fontDataCpy, fontData->Data, fontData->DataSize);
+            font = mImGuiIo->Fonts->AddFontFromMemoryTTF(fontDataCpy, fontData->DataSize, size);
+        }
     }
     // FontAwesome fonts need to have their sizes reduced by 2.0f/3.0f in order to align correctly
     float iconFontSize = size * 2.0f / 3.0f;
@@ -528,7 +533,6 @@ ArchiveVersion ReadPortVersionFromArchive(std::string archivePath, bool isO2rTyp
             version.minor = reader->ReadUInt16();
             version.patch = reader->ReadUInt16();
         }
-        archive->Close();
     }
 
     return version;
