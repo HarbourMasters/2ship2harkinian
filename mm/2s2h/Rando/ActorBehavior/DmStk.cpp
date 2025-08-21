829dac2c5738841c274d92eb70451e80b0437137
PurpleHato <linkvssangoku.jr@gmail.com>
2025-08-22 00:30:59 +0200

    Oopsie

--- mm/2s2h/Rando/ActorBehavior/DmStk.cpp
+++ mm/2s2h/Rando/ActorBehavior/DmStk.cpp
@@ -48,8 +48,7 @@ void ApplyOathHint(u16* textId, bool* loadFromMessageTable) {
             "J'entends la Mélodie des Géants ({{article}}%y{{itemName}}%w) venant de %y{{location}}%w. Mais il "
             "est trop tard! Ils ne peuvent plus t'aider!",
             "Ich kann die Giganten über %y{{article}}{{itemName}}%w singen hören, doch jetzt ist es sowieso zu spät "
-            "%y{{location}}%w danach zu suchen! Sie können dir nicht mehr helfen!"
-            ",
+            "%y{{location}}%w danach zu suchen! Sie können dir nicht mehr helfen!",
             "TODO_JAPANESE",
             "TODO_SPANISH");
         CustomMessage::Replace(&msg, "{{article}}", article);
