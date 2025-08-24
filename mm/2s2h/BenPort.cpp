fcbe3ec235006a1f7029627a8f120ab2cbbfa39a
louist103 <35883445+louist103@users.noreply.github.com>
2025-08-24 15:23:23 -0400

    Update BenPort.cpp

--- mm/2s2h/BenPort.cpp
+++ mm/2s2h/BenPort.cpp
@@ -738,7 +738,6 @@ extern "C" void InitOTR() {
     std::shared_ptr<Ship::Config> conf = OTRGlobals::Instance->context->GetConfig();
     Ship::Context::GetInstance()->GetFileDropMgr()->RegisterDropHandler(BinarySaveConverter_HandleFileDropped);
     Ship::Context::GetInstance()->GetFileDropMgr()->RegisterDropHandler(SaveManager_HandleFileDropped);
-    Ship::Context::GetInstance()->GetFileDropMgr()->RegisterDropHandler(SaveManager_HandleFileDropped);
 }
 
 extern "C" void SaveManager_ThreadPoolWait() {
