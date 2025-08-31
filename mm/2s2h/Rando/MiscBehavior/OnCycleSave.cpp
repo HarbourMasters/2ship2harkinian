a07bf794af957c74f1516dbd59057f0a239adb7c
Eblo <7004497+Eblo@users.noreply.github.com>
2025-08-30 22:42:54 -0400

    [Rando] Reset cycle collectible flags on cycle save (#1254)

--- mm/2s2h/Rando/MiscBehavior/OnCycleSave.cpp
+++ mm/2s2h/Rando/MiscBehavior/OnCycleSave.cpp
@@ -87,8 +87,17 @@ void Rando::MiscBehavior::AfterEndOfCycleSave() {
                         ~(1 << (randoStaticCheck.flag & 0x1F));
                 }
                 break;
-                // most of the others are handled by the game, with the exception of PERSISTENT_CYCLE_FLAGS_SET, not
-                // sure if any of these cases affect us yet so ignoring for now
+            case FLAG_CYCL_SCENE_COLLECTIBLE:
+                // Clear the flag without triggering hook
+                if (gPlayState->sceneId == randoStaticCheck.sceneId) {
+                    gPlayState->actorCtx.sceneFlags.collectible[(randoStaticCheck.flag & ~0x1F) >> 5] &=
+                        ~(1 << (randoStaticCheck.flag & 0x1F));
+                }
+                gSaveContext.save.saveInfo.permanentSceneFlags[randoStaticCheck.sceneId].collectible &=
+                    ~(1 << (randoStaticCheck.flag & 0x1F));
+                gSaveContext.cycleSceneFlags[randoStaticCheck.sceneId].collectible &=
+                    ~(1 << (randoStaticCheck.flag & 0x1F));
+                break;
         }
     }
 
