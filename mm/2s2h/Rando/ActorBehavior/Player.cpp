316230b2196f105b15c178a1fb25d83d0890ba50
Caladius <clatini88@gmail.com>
2025-09-25 09:51:52 -0400

    Feedback Updates

--- mm/2s2h/Rando/ActorBehavior/Player.cpp
+++ mm/2s2h/Rando/ActorBehavior/Player.cpp
@@ -8,8 +8,8 @@ extern "C" {
 #include "include/z64player.h"
 }
 
-void Swim_Ability(Player* player) {
-    // This is Honey & Darlings Shop, touching the water ends the minigame as its vanilla behaivor.
+void RespawnOnWaterTouch(Player* player) {
+    // This is Honey & Darlings Shop, touching the water ends the minigame as its vanilla behavior.
     // No reason to handle it a second time here.
     if (gPlayState->sceneId == SCENE_BOWLING) {
         return;
@@ -27,7 +27,7 @@ void Swim_Ability(Player* player) {
 void Rando::ActorBehavior::InitPlayerBehavior() {
     COND_ID_HOOK(OnActorUpdate, ACTOR_PLAYER, IS_RANDO && RANDO_SAVE_OPTIONS[RO_SHUFFLE_SWIM], [](Actor* actor) {
         if (!Flags_GetRandoInf(RANDO_INF_OBTAINED_SWIM)) {
-            Swim_Ability(GET_PLAYER(gPlayState));
+            RespawnOnWaterTouch(GET_PLAYER(gPlayState));
         }
     });
 }
