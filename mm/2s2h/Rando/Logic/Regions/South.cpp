28e063f4a8bc4439d2b858cc09af620c050bcd08
Eblo <7004497+Eblo@users.noreply.github.com>
2025-09-26 21:18:40 -0400

    Mimic Deku hop failure event when touching water
    Separate Woodfall owl statue platform into own region

--- mm/2s2h/Rando/Logic/Regions/South.cpp
+++ mm/2s2h/Rando/Logic/Regions/South.cpp
@@ -429,13 +429,8 @@ static RegisterShipInitFunc initFunc([]() {
     };
     Regions[RR_WOODFALL] = RandoRegion{ .sceneId = SCENE_21MITURINMAE,
         .checks = {
-            CHECK(RC_WOODFALL_ENTRANCE_CHEST, CAN_BE_DEKU || RANDO_EVENTS[RE_CLEARED_WOODFALL_TEMPLE]),
+            CHECK(RC_WOODFALL_ENTRANCE_CHEST, CAN_BE_DEKU || (RANDO_EVENTS[RE_CLEARED_WOODFALL_TEMPLE] && (CAN_BE_ZORA || CAN_BE_GORON || CAN_USE_ABILITY(SWIM)))),
             CHECK(RC_WOODFALL_PIECE_OF_HEART_CHEST, CAN_BE_DEKU),
-            CHECK(RC_WOODFALL_OWL_STATUE, CAN_USE_SWORD),
-            CHECK(RC_WOODFALL_NEAR_OWL_CHEST, CAN_BE_DEKU),
-            CHECK(RC_WOODFALL_POT_01, CAN_BE_DEKU || CAN_OWL_WARP(OWL_WARP_WOODFALL)),
-            CHECK(RC_WOODFALL_POT_02, CAN_BE_DEKU || CAN_OWL_WARP(OWL_WARP_WOODFALL)),
-            CHECK(RC_WOODFALL_POT_03, CAN_BE_DEKU || CAN_OWL_WARP(OWL_WARP_WOODFALL)),
             CHECK(RC_WOODFALL_FREESTANDING_RUPEE, CAN_BE_DEKU),
             CHECK(RC_WOODFALL_GRASS_01, true),
             CHECK(RC_WOODFALL_GRASS_02, true),
@@ -446,9 +441,26 @@ static RegisterShipInitFunc initFunc([]() {
         },
         .exits = { //     TO                                         FROM
             EXIT(ENTRANCE(SOUTHERN_SWAMP_POISONED, 2),      ENTRANCE(WOODFALL, 0), true),
-            EXIT(ENTRANCE(WOODFALL_TEMPLE, 0),              ENTRANCE(WOODFALL, 1), CanAccessDungeon(DUNGEON_INDEX_WOODFALL_TEMPLE)),
             EXIT(ENTRANCE(FAIRY_FOUNTAIN, 1),               ENTRANCE(WOODFALL, 2), CAN_BE_DEKU),
             EXIT(ENTRANCE(WOODFALL_TEMPLE, 2),              ENTRANCE(WOODFALL, 3), RANDO_EVENTS[RE_CLEARED_WOODFALL_TEMPLE]),
+        },
+        .connections = {
+            CONNECTION(RR_WOODFALL_OWL_STATUE_PLATFORM, CAN_BE_DEKU),
+        },
+    };
+    Regions[RR_WOODFALL_OWL_STATUE_PLATFORM] = RandoRegion{ .name = "Owl Statue Platform", .sceneId = SCENE_21MITURINMAE,
+        .checks = {
+            CHECK(RC_WOODFALL_OWL_STATUE, CAN_USE_SWORD),
+            CHECK(RC_WOODFALL_NEAR_OWL_CHEST, CAN_BE_DEKU),
+            CHECK(RC_WOODFALL_POT_01, true),
+            CHECK(RC_WOODFALL_POT_02, true),
+            CHECK(RC_WOODFALL_POT_03, true),
+        },
+        .exits = { //     TO                                         FROM
+            EXIT(ENTRANCE(WOODFALL_TEMPLE, 0),              ENTRANCE(WOODFALL, 1), CAN_BE_DEKU && CanAccessDungeon(DUNGEON_INDEX_WOODFALL_TEMPLE)),
+        },
+        .connections = {
+            CONNECTION(RR_WOODFALL, CAN_BE_DEKU),
         },
     };
     Regions[RR_WOODS_OF_MYSTERY_GROTTO] = RandoRegion{ .name = "Woods of Mystery Grotto", .sceneId = SCENE_KAKUSIANA,
