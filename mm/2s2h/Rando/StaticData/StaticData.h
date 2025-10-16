65c9b6eb3903615b7ebd3f957eb94ae81b7ddb6a
Caladius <clatini88@gmail.com>
2025-10-16 09:18:29 -0400

    Clean up 2

--- mm/2s2h/Rando/StaticData/StaticData.h
+++ mm/2s2h/Rando/StaticData/StaticData.h
@@ -44,7 +44,6 @@ struct RandoStaticItem {
 
 extern std::map<RandoItemId, RandoStaticItem> Items;
 extern std::unordered_map<StartingItemCategory, std::vector<RandoItemId>> StartingItemsMap;
-extern std::vector<RandoItemId> StartingItemsMap;
 
 RandoItemId GetItemIdFromName(const char* name);
 u8 GetIconForZMessage(RandoItemId itemId);
