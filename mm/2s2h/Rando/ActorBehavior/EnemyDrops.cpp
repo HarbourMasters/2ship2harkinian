763e1d845b2b0df6774f5760787cdc2a8972e739
Caladius <Caladius@users.noreply.github.com>
2025-10-28 13:53:02 -0400

    Update mm/2s2h/Rando/ActorBehavior/EnemyDrops.cpp
    Co-authored-by: Eblo <7004497+Eblo@users.noreply.github.com>

--- mm/2s2h/Rando/ActorBehavior/EnemyDrops.cpp
+++ mm/2s2h/Rando/ActorBehavior/EnemyDrops.cpp
@@ -68,10 +68,11 @@ Actor* FindActor(Vec3f position, ActorType actorType) {
 
     Actor* currentActor = actorList.first;
     for (size_t i = 0; i < actorList.length; i++) {
-        if (currentActor->world.pos.x != position.x && currentActor->world.pos.z != position.z) {
-            currentActor = currentActor->next;
-        } else {
+        if (currentActor->world.pos.x == position.x && currentActor->world.pos.y == position.y &&
+               currentActor->world.pos.z == position.z) {
             return currentActor;
+        } else {
+            currentActor = currentActor->next;
         }
     }
     return nullptr;
