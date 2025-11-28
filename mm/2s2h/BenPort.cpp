983d232cfbc7e7e1b6cacfb3bdced729c1098069
Glought <Glought@users.noreply.github.com>
2025-11-28 06:56:54 -0800

    Changed code that used "gDeveloperTools.*" to use the "CVAR_DEVELOPER_TOOLS("*")" macro.

--- mm/2s2h/BenPort.cpp
+++ mm/2s2h/BenPort.cpp
@@ -183,7 +183,7 @@ OTRGlobals::OTRGlobals() {
 #else
     auto defaultLogLevel = spdlog::level::info;
 #endif
-    auto logLevel = (spdlog::level::level_enum)CVarGetInteger("gDeveloperTools.LogLevel", defaultLogLevel);
+    auto logLevel = (spdlog::level::level_enum)CVarGetInteger(CVAR_DEVELOPER_TOOLS("LogLevel"), defaultLogLevel);
     context->InitLogging(logLevel, logLevel);
     Ship::Context::GetInstance()->GetLogger()->set_pattern("[%H:%M:%S.%e] [%s:%#] [%l] %v");
 
