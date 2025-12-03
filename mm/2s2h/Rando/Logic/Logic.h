956c47e317a8579c28b468055ea662dd3646bf96
Caladius <Caladius@users.noreply.github.com>
2025-12-03 13:55:03 -0500

    Update mm/2s2h/Rando/Logic/Logic.h
    Co-authored-by: Eblo <7004497+Eblo@users.noreply.github.com>

--- mm/2s2h/Rando/Logic/Logic.h
+++ mm/2s2h/Rando/Logic/Logic.h
@@ -213,7 +213,6 @@ inline bool canPlaySong(u8 songId) {
                     Flags_GetRandoInf(RANDO_INF_OBTAINED_OCARINA_BUTTON_C_LEFT));
         case OCARINA_SONG_SCARECROW_SPAWN:
             return FoundOcarinaButtons() >= 2;
-            break;
         default:
             return true;
     }
