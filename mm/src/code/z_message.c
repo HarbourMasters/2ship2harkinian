0d7e5fc8049e9dbde507193c5a3d4cf87395ee0f
Jordan Longstaff <JordanLongstaff@users.noreply.github.com>
2025-12-13 15:04:05 -0500

    Name four global variables in `z_message.c` (#1832)
    * Name static arrays for SoDT skip message IDs
    
    * Rename two more static variables
    
    * Convert hex drop thresholds to decimal
    
    * Rename to text IDs

--- mm/src/code/z_message.c
+++ mm/src/code/z_message.c
@@ -166,7 +166,7 @@ const char* gStaticItemIcons[] = {
 };
 // #endregion
 
-u8 D_801C6A70 = 0;
+u8 sMessageStartFrameCount = 0;
 s16 sOcarinaButtonIndexBufPos = 0;
 s16 sOcarinaButtonIndexBufLen = 0;
 s16 sLastPlayedSong = 0xFF;
@@ -187,7 +187,7 @@ s16 sOcarinaButtonStepG = 0;
 s16 sOcarinaButtonStepB = 0;
 s16 sOcarinaButtonFlashTimer = 12;
 s16 sOcarinaButtonFlashColorIndex = 1;
-s16 D_801C6A94 = 0;
+s16 sOcarinaButtonDropYOffset = 0;
 
 u8 gPageSwitchNextButtonStatus[][5] = {
     { BTN_ENABLED, BTN_ENABLED, BTN_DISABLED, BTN_ENABLED, BTN_ENABLED },
@@ -3374,7 +3374,7 @@ void Message_OpenText(PlayState* play, u16 textId) {
     }
 
     msgCtx->messageHasSetSfx = false;
-    D_801C6A70 = 0;
+    sMessageStartFrameCount = 0;
     msgCtx->textboxSkipped = false;
     msgCtx->textIsCredits = false;
     var_fv0 = 1.0f;
@@ -3494,7 +3494,7 @@ void Message_PauseMenu_ShowDescription(PlayState* play, u16 textId, u8 textBoxPo
         gSaveContext.prevHudVisibility = gSaveContext.hudVisibility;
     }
     msgCtx->messageHasSetSfx = false;
-    D_801C6A70 = 0;
+    sMessageStartFrameCount = 0;
     msgCtx->textboxSkipped = false;
     msgCtx->textIsCredits = false;
 
@@ -4780,7 +4780,7 @@ void Message_DrawMain(PlayState* play, Gfx** gfxP) {
             case MSGMODE_OCARINA_FAIL_NO_TEXT:
                 msgCtx->stateTimer--;
                 if (msgCtx->stateTimer == 0) {
-                    D_801C6A94 = 1;
+                    sOcarinaButtonDropYOffset = 1;
                     if (msgCtx->msgMode == MSGMODE_SONG_PROMPT_FAIL) {
                         Message_ContinueTextbox(play, 0x1B89);
                         Message_Decode(play);
@@ -4794,10 +4794,10 @@ void Message_DrawMain(PlayState* play, Gfx** gfxP) {
             case MSGMODE_OCARINA_NOTES_DROP:
             case MSGMODE_SONG_PROMPT_NOTES_DROP:
                 for (i = 0; i < 5; i++) {
-                    msgCtx->ocarinaButtonsPosY[i] += D_801C6A94;
+                    msgCtx->ocarinaButtonsPosY[i] += sOcarinaButtonDropYOffset;
                 }
-                D_801C6A94 += D_801C6A94;
-                if (D_801C6A94 >= 0x226) {
+                sOcarinaButtonDropYOffset += sOcarinaButtonDropYOffset;
+                if (sOcarinaButtonDropYOffset >= 550) {
                     Message_ResetOcarinaButtonAlphas();
                     if (msgCtx->msgMode == MSGMODE_SONG_PROMPT_NOTES_DROP) {
                         msgCtx->msgMode = MSGMODE_OCARINA_AWAIT_INPUT;
@@ -5206,15 +5206,15 @@ void Message_DrawMain(PlayState* play, Gfx** gfxP) {
                     ocarinaError = func_8019B5AC();
                     if (ocarinaError == OCARINA_ERROR_2) {
                         Audio_PlaySfx(NA_SE_SY_OCARINA_ERROR);
-                        D_801C6A94 = 1;
+                        sOcarinaButtonDropYOffset = 1;
                         msgCtx->msgMode = MSGMODE_3B;
                     } else if (ocarinaError == OCARINA_ERROR_3) {
                         Audio_PlaySfx(NA_SE_SY_OCARINA_ERROR);
-                        D_801C6A94 = 1;
+                        sOcarinaButtonDropYOffset = 1;
                         msgCtx->msgMode = MSGMODE_3E;
                     } else {
                         Audio_PlaySfx(NA_SE_SY_OCARINA_ERROR);
-                        D_801C6A94 = 1;
+                        sOcarinaButtonDropYOffset = 1;
                         msgCtx->msgMode = MSGMODE_38;
                     }
                 }
@@ -5231,11 +5231,11 @@ void Message_DrawMain(PlayState* play, Gfx** gfxP) {
             case MSGMODE_3E:
                 // notes drop
                 for (i = 0; i < 5; i++) {
-                    msgCtx->ocarinaButtonsPosY[i] += D_801C6A94;
+                    msgCtx->ocarinaButtonsPosY[i] += sOcarinaButtonDropYOffset;
                 }
 
-                D_801C6A94 += D_801C6A94;
-                if (D_801C6A94 >= 0x226) {
+                sOcarinaButtonDropYOffset += sOcarinaButtonDropYOffset;
+                if (sOcarinaButtonDropYOffset >= 550) {
                     Message_ResetOcarinaButtonAlphas();
                     msgCtx->textBoxType = TEXTBOX_TYPE_BLACK;
                     msgCtx->textboxColorRed = msgCtx->textboxColorGreen = msgCtx->textboxColorBlue = 0;
@@ -5488,14 +5488,14 @@ s16 sTextboxMidYPositions[] = {
 
 s16 D_801D0448[] = { 0x1C, 0x1D, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25 };
 
-s16 D_801D045C[] = {
-    0x1B91,
-    0x1B90,
-    0x1B8F,
+s16 sDoubleTimeSkipToNightTextIds[] = {
+    0x1B91, // Night of 1st day
+    0x1B90, // Night of 2nd day
+    0x1B8F, // Night of final day
 };
-s16 D_801D0464[] = {
-    0x1B92,
-    0x1B8E,
+s16 sDoubleTimeSkipToDayTextIds[] = {
+    0x1B92, // Dawn of 2nd day
+    0x1B8E, // Dawn of final day
 };
 
 void Message_Update(PlayState* play) {
@@ -5598,10 +5598,10 @@ void Message_Update(PlayState* play) {
 
     switch (msgCtx->msgMode) {
         case MSGMODE_TEXT_START:
-            D_801C6A70++;
+            sMessageStartFrameCount++;
 
             temp = false;
-            if ((D_801C6A70 >= 4) || ((msgCtx->talkActor == NULL) && (D_801C6A70 >= 2))) {
+            if ((sMessageStartFrameCount >= 4) || ((msgCtx->talkActor == NULL) && (sMessageStartFrameCount >= 2))) {
                 temp = true;
             }
             if (temp) {
@@ -6109,9 +6109,9 @@ void Message_Update(PlayState* play) {
                                                   (CURRENT_DAY != 3) || (gSaveContext.save.isNight == 0))) {
                             if (GameInteractor_Should(VB_DISPLAY_SONG_OF_DOUBLE_TIME_PROMPT, true)) {
                                 if (gSaveContext.save.isNight) {
-                                    Message_StartTextbox(play, D_801D0464[CURRENT_DAY - 1], NULL);
+                                    Message_StartTextbox(play, sDoubleTimeSkipToDayTextIds[CURRENT_DAY - 1], NULL);
                                 } else {
-                                    Message_StartTextbox(play, D_801D045C[CURRENT_DAY - 1], NULL);
+                                    Message_StartTextbox(play, sDoubleTimeSkipToNightTextIds[CURRENT_DAY - 1], NULL);
                                 }
                                 play->msgCtx.ocarinaMode = OCARINA_MODE_PROCESS_DOUBLE_TIME;
                             }
