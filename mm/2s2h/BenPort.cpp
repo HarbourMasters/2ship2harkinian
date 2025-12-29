4ae48ce5950d89b0d852ecc7b9e7033bd649bf32
Laqueeshous <42100286+Jameriquiah@users.noreply.github.com>
2025-12-29 09:09:15 -0500

    Crash fix + Deku and Goron

--- mm/2s2h/BenPort.cpp
+++ mm/2s2h/BenPort.cpp
@@ -53,7 +53,7 @@ CrowdControl* CrowdControl::Instance;
 #include "2s2h/GameInteractor/GameInteractor.h"
 #include "2s2h/Enhancements/Enhancements.h"
 #include "2s2h/Enhancements/GfxPatcher/AuthenticGfxPatches.h"
-#include "2s2h/Enhancements/GfxPatcher/PlayerFDFlipbooks.h"
+#include "2s2h/Enhancements/GfxPatcher/PlayerCustomFlipbooks.h"
 #include "2s2h/DeveloperTools/DebugConsole.h"
 #include "2s2h/Rando/Rando.h"
 #include "2s2h/Rando/Spoiler/Spoiler.h"
@@ -720,7 +720,7 @@ extern "C" void InitOTR() {
     OTRMessage_Init();
     OTRAudio_Init();
     OTRExtScanner();
-    PlayerFDFlipbooks_Patch();
+    PlayerCustomFlipbooks_Patch();
 
     // Just came up with arbitrary numbers that seemed to work, this is
     // usually set once(?) in currently stubbed out areas of code.
@@ -1967,3 +1967,4 @@ extern "C" bool Ship_HandleConsoleCrashAsReset() {
 
     return true;
 }
+
--- mm/2s2h/Enhancements/GfxPatcher/PlayerFDFlipbooks.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void PlayerFDFlipbooks_Patch(void);
