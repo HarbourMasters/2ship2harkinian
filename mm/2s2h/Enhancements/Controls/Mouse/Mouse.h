1480eff942f2780a4789a2d78d1ad7fdaaa1eec8
lightmanLP <liteman1000@gmail.com>
2026-01-26 19:54:40 +0700

    better auto mouse capture proto

--- mm/2s2h/Enhancements/Controls/Mouse/Mouse.h
+++ mm/2s2h/Enhancements/Controls/Mouse/Mouse.h
@@ -12,11 +12,20 @@ typedef struct MouseCoords {
     s32 y;
 } MouseCoords;
 
+typedef struct MouseCaptureGameState {
+    bool inKaleido;
+    bool gameStarted;
+    bool isCaptureForced;
+    bool forcedCaptureState;
+} MouseCaptureGameState;
+
 void Mouse_Update();
 MouseCoords Mouse_GetDelta();
 MouseCoords Mouse_GetPos();
 void Mouse_SetCursorPos(s32 x, s32 y);
 bool Mouse_IsCaptured();
+void Mouse_ForceToggleCapture();
+void Mouse_UpdateCaptureByState();
 
 #ifdef __cplusplus
 } // extern "C"
