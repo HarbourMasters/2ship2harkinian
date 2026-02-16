f7259a413588a61ab4e194e92ba65d164330bc8b
lightmanLP <liteman1000@gmail.com>
2026-02-16 08:11:04 +0700

    another fix

--- mm/2s2h/Enhancements/Controls/Mouse/LocalMouseStateManager.cpp
+++ mm/2s2h/Enhancements/Controls/Mouse/LocalMouseStateManager.cpp
@@ -12,6 +12,6 @@ void LocalMouseStateManager::UpdateMouseCapture() {
     Mouse_UpdateCaptureByState();
     auto wnd = std::dynamic_pointer_cast<Fast::Fast3dWindow>(Ship::Context::GetInstance()->GetWindow());
     if (wnd->IsMouseCaptured()) {
-        SetCursorVisibleTicks(GetCursorVisibilityTime() * wnd->GetTargetFps());
+        ResetCursorVisibilityTimer();
     }
 }
