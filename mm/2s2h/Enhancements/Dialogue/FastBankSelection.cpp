e8cc3678b970c0765455369fe9cdcd7cc198889b
mckinlee <mckinlee@ymail.com>
2026-03-17 18:00:52 -0400

    feedback

--- mm/2s2h/Enhancements/Dialogue/FastBankSelection.cpp
+++ mm/2s2h/Enhancements/Dialogue/FastBankSelection.cpp
@@ -24,33 +24,23 @@ void FastBankSelection_UpdateMessage(const char rupeeValue[3]) {
 void OnEnGinkoManUpdate(Actor* actor) {
     EnGinkoMan* enGinko = (EnGinkoMan*)actor;
     if (gPlayState->msgCtx.currentTextId == 0x450) {
+        uint32_t depositAmount = MIN(gSaveContext.save.saveInfo.playerData.rupees, 999u);
         if (CHECK_BTN_ALL(gPlayState->state.input[0].cur.button, BTN_Z) &&
-            gPlayState->msgCtx.rupeesSelected != gSaveContext.save.saveInfo.playerData.rupees) {
-            char firstChar = (gSaveContext.save.saveInfo.playerData.rupees / 100) + '0';
-            char secondChar = ((gSaveContext.save.saveInfo.playerData.rupees / 10) % 10) + '0';
-            char thirdChar = (gSaveContext.save.saveInfo.playerData.rupees % 10) + '0';
+            gPlayState->msgCtx.rupeesSelected != (s32)depositAmount) {
+            char firstChar = (depositAmount / 100) + '0';
+            char secondChar = ((depositAmount / 10) % 10) + '0';
+            char thirdChar = (depositAmount % 10) + '0';
             const char rupeeChar[3] = { firstChar, secondChar, thirdChar };
 
             FastBankSelection_UpdateMessage(rupeeChar);
         }
     } else if (gPlayState->msgCtx.currentTextId == 0x46E) {
-        uint32_t walletSize = CUR_UPG_VALUE(UPG_WALLET);
         uint32_t currentRupees = gSaveContext.save.saveInfo.playerData.rupees;
         uint32_t maxWallet;
         if (CHECK_BTN_ALL(gPlayState->state.input[0].cur.button, BTN_Z)) {
-            switch (ITEM_WALLET_DEFAULT + walletSize) {
-                case ITEM_WALLET_ADULT:
-                    maxWallet = (200 - currentRupees);
-                    break;
-                case ITEM_WALLET_GIANT:
-                    maxWallet = (500 - currentRupees);
-                    break;
-                default:
-                    maxWallet = (99 - currentRupees);
-                    break;
-            }
-
-            maxWallet = MIN(maxWallet, HS_GET_BANK_RUPEES());
+            uint32_t walletCapacity = CUR_CAPACITY(UPG_WALLET);
+            maxWallet = (walletCapacity > currentRupees) ? (walletCapacity - currentRupees) : 0;
+            maxWallet = MIN(maxWallet, MIN(HS_GET_BANK_RUPEES(), 999u));
 
             if (maxWallet != 0 && gPlayState->msgCtx.rupeesSelected != maxWallet) {
                 char firstChar = (maxWallet / 100) + '0';
