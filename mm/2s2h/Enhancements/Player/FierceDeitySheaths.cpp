cedd77b5c297af196b0e674a2ef3857c7f84ee15
Jameriquiah <jordynhardyman12@gmail.com>
2026-03-20 20:06:02 -0400

    cleanup

--- mm/2s2h/Enhancements/Player/FierceDeitySheaths.cpp
+++ mm/2s2h/Enhancements/Player/FierceDeitySheaths.cpp
@@ -10,64 +10,55 @@ Gfx* ResourceMgr_LoadGfxByName(const char* path);
 
 static const char* sFierceDeitySwordInSheathDLPath = "__OTR__objects/object_link_boy/gLinkFierceDeitySwordInSheathDL";
 static const char* sFierceDeitySwordSheathDLPath = "__OTR__objects/object_link_boy/gLinkFierceDeitySwordSheathDL";
-static Gfx* sFierceDeitySwordInSheathDL = nullptr;
-static Gfx* sFierceDeitySwordSheathDL = nullptr;
-static s32 sFierceDeitySheathState = 0;
+static Gfx* sFierceDeitySwordInSheathDLs[2] = { nullptr, nullptr };
+static Gfx* sFierceDeitySwordSheathDLs[2] = { nullptr, nullptr };
+
+static bool sFierceDeitySheathChecked = false;
 
 static bool CustomFDSheath_LoadAssets() {
-    if (sFierceDeitySheathState == 0) {
-        sFierceDeitySheathState = 1;
+    if (!sFierceDeitySheathChecked) {
+        sFierceDeitySheathChecked = true;
 
         if (ResourceMgr_FileExists(sFierceDeitySwordInSheathDLPath) &&
             ResourceMgr_FileExists(sFierceDeitySwordSheathDLPath)) {
-            sFierceDeitySwordInSheathDL = ResourceMgr_LoadGfxByName(sFierceDeitySwordInSheathDLPath);
-            sFierceDeitySwordSheathDL = ResourceMgr_LoadGfxByName(sFierceDeitySwordSheathDLPath);
-            sFierceDeitySheathState = 1;
+            sFierceDeitySwordInSheathDLs[0] = sFierceDeitySwordInSheathDLs[1] =
+                ResourceMgr_LoadGfxByName(sFierceDeitySwordInSheathDLPath);
+            sFierceDeitySwordSheathDLs[0] = sFierceDeitySwordSheathDLs[1] =
+                ResourceMgr_LoadGfxByName(sFierceDeitySwordSheathDLPath);
         }
     }
 
-    return sFierceDeitySheathState == 1;
+    return (sFierceDeitySwordInSheathDLs[0] != nullptr) && (sFierceDeitySwordSheathDLs[0] != nullptr);
 }
 
-static Gfx* CustomFDSheath_GetDList(Player* player) {
+static Gfx** CustomFDSheath_GetDLists(Player* player) {
     if (!CustomFDSheath_LoadAssets()) {
         return nullptr;
     }
 
     if (player->modelGroup == PLAYER_MODELGROUP_TWO_HAND_SWORD) {
-        return sFierceDeitySwordSheathDL;
+        return sFierceDeitySwordSheathDLs;
     }
 
-    return sFierceDeitySwordInSheathDL;
+    return sFierceDeitySwordInSheathDLs;
 }
 
 void RegisterCustomFDSheath() {
-    GameInteractor::Instance->RegisterGameHookForID<GameInteractor::OnPlayerPostLimbDraw>(
-        PLAYER_LIMB_SHEATH, [](Player* player, s32) {
-            Gfx* sheathDList;
+    GameInteractor::Instance->RegisterGameHookForID<GameInteractor::OnActorUpdate>(ACTOR_PLAYER, [](Actor* actor) {
+        Player* player = (Player*)actor;
+        Gfx** sheathDLists;
 
-            if (player->transformation != PLAYER_FORM_FIERCE_DEITY) {
-                return;
-            }
+        if (player->transformation != PLAYER_FORM_FIERCE_DEITY) {
+            return;
+        }
 
-            sheathDList = CustomFDSheath_GetDList(player);
-            if (sheathDList == nullptr) {
-                return;
-            }
+        sheathDLists = CustomFDSheath_GetDLists(player);
+        if (sheathDLists == nullptr) {
+            return;
+        }
 
-            OPEN_DISPS(gPlayState->state.gfxCtx);
-
-            Matrix_Push();
-            // FD sheath seems to be rotated 180 degrees ingame for some reason?
-            Matrix_RotateZS(0x8000, MTXMODE_APPLY);
-            // FD sheath is a tad lower on the player's back than it should be
-            Matrix_Translate(-600.0f, 0.0f, 0.0f, MTXMODE_APPLY);
-            MATRIX_FINALIZE_AND_LOAD(POLY_OPA_DISP++, gPlayState->state.gfxCtx);
-            gSPDisplayList(POLY_OPA_DISP++, sheathDList);
-            Matrix_Pop();
-
-            CLOSE_DISPS(gPlayState->state.gfxCtx);
-        });
+        player->sheathDLists = sheathDLists;
+    });
 }
 
 static RegisterShipInitFunc initFunc(RegisterCustomFDSheath, {});
