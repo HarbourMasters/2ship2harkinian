8154fb58b02654bde8f4c3ca5cf9f5585afa0826
JordanLongstaff <jrdnlngstff@gmail.com>
2026-04-11 07:52:23 -0400

    Add missing include

--- mm/2s2h/Enhancements/Dialogue/SkipOwlSaveWarning.cpp
+++ mm/2s2h/Enhancements/Dialogue/SkipOwlSaveWarning.cpp
@@ -1,7 +1,8 @@
 #include <libultraship/bridge/consolevariablebridge.h>
-#include "2s2h/GameInteractor/GameInteractor.h"
-#include "2s2h/ShipInit.hpp"
 #include "2s2h/CustomMessage/CustomMessage.h"
+#include "2s2h/GameInteractor/GameInteractor.h"
+#include "2s2h/BenPort.hpp"
+#include "2s2h/ShipInit.hpp"
 
 #define CVAR_NAME "gEnhancements.Saving.PersistentOwlSaves"
 #define CVAR CVarGetInteger(CVAR_NAME, 0)
