9ca95bb4b8c06a2c6eadba41c070384209fa9bb8
cardinalnerd <the.cardinalnerd@cardinalnerd.com>
2026-06-25 06:26:11 -0500

    Update OnRaceInit.cpp
    Fixing Errors

--- mm/2s2h/Rando/MiscBehavior/OnRaceInit.cpp
+++ mm/2s2h/Rando/MiscBehavior/OnRaceInit.cpp
@@ -8,12 +8,12 @@ extern "C" {
 static bool isRaceInitialized = false;
 
 void Rando::MiscBehavior::OnRaceFileInit() {
-    COND_HOOK(OnSaveLoad, RANDO_SAVE_OPTIONS[RO_LOGIC] == RO_LOGIC_VOYAGE3,
+    COND_HOOK(OnSaveLoad, RANDO_SAVE_OPTIONS[RO_LOGIC] == RO_LOGIC_VOYAGE_3,
               [](s16 fileNum) { isRaceInitialized = false; });
 
     COND_HOOK(OnSceneInit, IS_RANDO, [](s8 sceneId, s8 spawnNum) {
         if (!isRaceInitialized) {
-            if (RANDO_SAVE_OPTIONS[RO_LOGIC] == RO_LOGIC_VOYAGE3) {
+            if (RANDO_SAVE_OPTIONS[RO_LOGIC] == RO_LOGIC_VOYAGE_3) {
                 PresetManager_ApplyPreset(voyage3J);
             }
             isRaceInitialized = true;
