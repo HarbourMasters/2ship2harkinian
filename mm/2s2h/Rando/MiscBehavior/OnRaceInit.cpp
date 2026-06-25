05b5aeec49d7af0fdae9d8129476c1c8da9c96e8
cardinalnerd <the.cardinalnerd@cardinalnerd.com>
2026-06-25 05:42:57 -0500

    Create OnRaceInit.cpp
    Attempting to add Caladius's reload of settings on file load.

---
+++ mm/2s2h/Rando/MiscBehavior/OnRaceInit.cpp
@@ -0,0 +1,22 @@
+#include "MiscBehavior.h"
+#include "PresetManager/PresetManager.h"
+
+extern "C" {
+#include "variables.h"
+}
+
+static bool isRaceInitialized = false;
+
+void Rando::MiscBehavior::OnRaceFileInit() {
+    COND_HOOK(OnSaveLoad, RANDO_SAVE_OPTIONS[RO_LOGIC] == RO_LOGIC_VOYAGE3,
+              [](s16 fileNum) { isRaceInitialized = false; });
+
+    COND_HOOK(OnSceneInit, IS_RANDO, [](s8 sceneId, s8 spawnNum) {
+        if (!isRaceInitialized) {
+            if (RANDO_SAVE_OPTIONS[RO_LOGIC] == RO_LOGIC_VOYAGE3) {
+                PresetManager_ApplyPreset(voyage3J);
+            }
+            isRaceInitialized = true;
+        }
+    });
+}
