573659f0dd95a6837cffa5424018b38ce5f1d115
Garrett <garrettjcox@gmail.com>
2026-07-03 17:10:10 -0500

    Expose rando inf flags in save editor

--- mm/2s2h/BenGui/UIWidgets.hpp
+++ mm/2s2h/BenGui/UIWidgets.hpp
@@ -1183,6 +1183,7 @@ bool StateButton(const char* str_id, const char* label, ImVec2 size, UIWidgets::
 void DrawFlagArray32(const std::string& name, uint32_t& flags, Colors color = Colors::LightBlue);
 void DrawFlagArray16(const std::string& name, uint16_t& flags, Colors color = Colors::LightBlue);
 void DrawFlagTableArray16(const FlagTable& flagTable, uint16_t& flags);
+void DrawFlagTableArray16(const FlagTable& flagTable, uint16_t row, uint16_t& flags);
 void DrawFlagTableArray8(const FlagTable& flagTable, uint16_t row, uint8_t& flags);
 void DrawFlagTableArray8Mask(const FlagTable& flagTable, uint16_t row, uint8_t& flags);
 bool BtnSelector(const char* label, int32_t* value, const BtnSelectorOptions& options);
