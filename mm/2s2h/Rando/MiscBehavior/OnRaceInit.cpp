4cdbf169925a2c92ff9bb145d295269b1ddb9554
cardinalnerd <the.cardinalnerd@cardinalnerd.com>
2026-07-09 19:24:18 -0500

    Update Voyage 3 Preset Name

--- mm/2s2h/Rando/MiscBehavior/OnRaceInit.cpp
+++ mm/2s2h/Rando/MiscBehavior/OnRaceInit.cpp
@@ -14,7 +14,7 @@ void Rando::MiscBehavior::OnRaceFileInit() {
     COND_HOOK(OnSceneInit, IS_RANDO, [](s8 sceneId, s8 spawnNum) {
         if (!isRaceInitialized) {
             if (RANDO_SAVE_OPTIONS[RO_LOGIC] == RO_LOGIC_VOYAGE_3) {
-                PresetManager_ApplyPreset(voyage3J);
+                PresetManager_ApplyPreset(voyage3PresetJ);
             }
             isRaceInitialized = true;
         }
