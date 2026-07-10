3b9afed170cfecd9355bb069a4871eede6de3ccd
cardinalnerd <the.cardinalnerd@cardinalnerd.com>
2026-07-09 23:20:37 -0500

    Updated Voyage 3 Preset

--- mm/2s2h/PresetManager/PresetManager.cpp
+++ mm/2s2h/PresetManager/PresetManager.cpp
@@ -308,6 +308,11 @@ nlohmann::json voyage3PresetJ = R"(
             }
         },
         "gEnhancements": {
+			"Camera": {
+                "DebugCam": {
+                    "Enable": 0
+                }
+            },
             "Cutscenes": {
                 "HideTitleCards": 1,
                 "SkipEnemyCutscenes": 1,
@@ -321,12 +326,17 @@ nlohmann::json voyage3PresetJ = R"(
                 "SkipToFileSelect": 1
             },
             "Cycle": {
+                "DoNotResetBottleContent": 0,
                 "DoNotResetChateau": 0,
                 "DoNotResetConsumables": 0,
                 "DoNotResetRazorSword": 0,
                 "DoNotResetRupees": 0,
+                "DoNotResetScarecrowSong": 0,
                 "DoNotResetTimeSpeed": 0,
+                "KeepExpressMail": 0,
+                "OceansideWalletAnyDay": 0,
                 "SaveOnMoonCrash": 0,
+                "StopOceansideSpiderHouseSquatter": 0,
                 "TingleAlwaysInClockTown": 0
             },
             "Dialogue": {
@@ -336,6 +346,7 @@ nlohmann::json voyage3PresetJ = R"(
                 "SkipBottlePickupMessages": 1
             },
             "DifficultyOptions": {
+				"BossHealthMultiplier": 0,
                 "DamageMultiplier": 0,
                 "DekuGuardSearchBalls": 0,
                 "DeleteFileOnDeath": 0,
@@ -367,6 +378,7 @@ nlohmann::json voyage3PresetJ = R"(
                 "CompletedHeartContainerAudio": 1,
                 "ConsoleCrashes": 1,
                 "ControlCharacters": 1,
+				"DekuButlerFixShockLoopAnimation": 0,
                 "FierceDeityZTargetMovement": 1,
                 "FixTexturesOOB": 1
             },
@@ -375,6 +387,7 @@ nlohmann::json voyage3PresetJ = R"(
             },
             "Graphics": {
                 "3DItemDrops": 1,
+				"ActorCullingAccountsForWidescreen": 0,
                 "AuthenticLogo": 1,
                 "DisableBlackBars": 1,
                 "FixSceneGeometrySeams": 0,
@@ -415,7 +428,9 @@ nlohmann::json voyage3PresetJ = R"(
                 "HoneyAndDarlingDay1": 8,
                 "HoneyAndDarlingDay2": 8,
                 "HoneyAndDarlingDay3": 16,
+				"RandomizeShootingGalleryOctoroks": 0,
                 "RomaniTargetPractice": 10,
+				"SkipHorseRace": 0,
                 "SkipLittleBeaver": 1,
                 "SwampArcheryScore": 2120,
                 "SwordsmanSchoolScore": 30,
@@ -452,8 +467,10 @@ nlohmann::json voyage3PresetJ = R"(
                 "ConstantFlipsHops": 0,
                 "JPGrottos": 0,
                 "OoTFasterSwim": 0,
+				"PauseBufferWindow": 0,
                 "PowerCrouchStab": 2,
                 "SideRoll": 0,
+				"SoilPatch": 1,
                 "TatlISG": 1,
                 "WoodfallMountainAppearance": 1
             },
@@ -571,7 +588,13 @@ nlohmann::json voyage3PresetJ = R"(
                 "Freeze": 1,
                 "Shock": 1
             }
-        }
+        },
+		"gSettings": {
+			"Menu": {
+				"Popout": 0
+			},
+			"SimulatedInputLag": 0
+		}
     },
     "type": "2S2H_PRESET",
     "version": 1
