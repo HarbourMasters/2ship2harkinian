c9416c7b3ca2cd521f5341697fde3b3cd1315b2c
lightmanLP <liteman1000@gmail.com>
2026-08-09 07:39:25 +0700

    active item on B

--- mm/2s2h/BenGui/BenMenu.cpp
+++ mm/2s2h/BenGui/BenMenu.cpp
@@ -1181,6 +1181,11 @@ void BenMenu::AddEnhancements() {
         .CVar("gEnhancements.Player.InvertZoraSwimY")
         .Options(CheckboxOptions().DefaultValue(true).Tooltip(
             "Invert the Y axis while swimming as Zora with the left stick."));
+    AddWidget(path, "Active Item on B", WIDGET_CVAR_CHECKBOX)
+        .CVar("gEnhancements.Equipment.ActiveItemOnB")
+        .Options(CheckboxOptions().Tooltip(
+            "Press B to use the item currently held by Link (Bow, Hookshot, Great Fairy's Sword and Deku Stick). "
+            "Second press on item unequips it."));
 
     path.column = SECTION_COLUMN_2;
     AddWidget(path, "Modes", WIDGET_SEPARATOR_TEXT);
